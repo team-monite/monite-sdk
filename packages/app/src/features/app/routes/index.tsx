059ef26904f9fd7cfa2e84011250f32f34931e45
Yuliya Ayubova <yuliya.ayubova@monite.com>
2022-08-16 15:24:45 +0300

    feat: move payment page to another package

--- packages/app/src/features/app/routes/index.tsx
+++ packages/app/src/features/app/routes/index.tsx
@@ -2,11 +2,7 @@ import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { observer } from 'mobx-react-lite';
 
-import { ROUTES } from 'features/app/consts';
 import { useRootStore } from 'features/mobx';
-
-import ReceivablePaymentPage from 'features/pay/PaymentPage';
-import ReceivablePaymentResultPage from 'features/pay/PaymentResultPage';
 
 const PublicArea = lazy(() => import('./PublicArea'));
 const AuthorizedArea = lazy(() => import('./AuthorizedArea'));
@@ -25,11 +21,6 @@ const AreaRoutes = observer(() => {
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path={'pay/*'} element={<ReceivablePaymentPage />} />
-      <Route
-        path={ROUTES.payResult}
-        element={<ReceivablePaymentResultPage />}
-      />
       <Route path="*" element={<AreaRoutes />} />
     </Routes>
   );
