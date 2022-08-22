f4155fb4ca045a0f1009be85636dc2334957567b
Nikita Tokarev <nikita.tokarev@monite.com>
2022-08-22 15:15:16 +0400

    DEV-3180: Fixed Kit build

--- packages/kit/src/components/index.ts
+++ packages/kit/src/components/index.ts
@@ -1,7 +1,6 @@
 export * from './counterparts/CounterpartsTable';
 export { default as CounterpartsFormCreate } from './counterparts/FormCreate';
 export { default as CounterpartDetails } from './counterparts/CounterpartDetails';
-export * from './payment/FileViewer';
 
 export * from './approvalPolicies/ApprovalPoliciesTable';
 export * from './approvalPolicies/ApprovalPolicyCreate';
