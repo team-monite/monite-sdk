708b08c60fdc625b8743304efd2d228e6d555491
Nikita Tokarev <nikita.tokarev@monite.com>
2022-08-23 18:16:29 +0400

    DEV-3065: PayableDetails integrated to WL

--- packages/app/src/features/payables/PagePayables/index.tsx
+++ packages/app/src/features/payables/PagePayables/index.tsx
@@ -1,16 +1,1 @@
-import React from 'react';
-import { PayablesTableWithAPI } from '@monite/react-kit';
-
-import Layout from 'features/app/Layout';
-import PageHeader from 'features/app/Layout/PageHeader';
-
-const PagePayables = () => {
-  return (
-    <Layout>
-      <PageHeader title="Payables" />
-      <PayablesTableWithAPI />
-    </Layout>
-  );
-};
-
-export default PagePayables;
+export { default } from './PagePayables';
--- packages/kit/src/components/index.ts
+++ packages/kit/src/components/index.ts
@@ -1,6 +1,7 @@
 export * from './counterparts/CounterpartsTable';
 export { default as CounterpartsFormCreate } from './counterparts/FormCreate';
 export { default as CounterpartDetails } from './counterparts/CounterpartDetails';
+export { default as PayableDetails } from './payables/PayableDetails';
 
 export * from './approvalPolicies/ApprovalPoliciesTable';
 export * from './approvalPolicies/ApprovalPolicyCreate';
