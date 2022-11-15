856e47643caa18545d695535cc3b092ed1c5ab42
Julia Ayubova <juliaayubova@Alvis-MacBook-Pro.local>
2022-11-15 19:36:14 +0300

    fix: fee is not shown for payer

---
+++ packages/sdk-api/src/api/models/PaymentsPaymentMethodsCalculatePaymentsPaymentsFeeResponse.ts
@@ -0,0 +1,12 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { PaymentsPaymentsPaidBy } from './PaymentsPaymentsPaidBy';
+import type { PaymentsTotal } from './PaymentsTotal';
+
+export type PaymentsPaymentMethodsCalculatePaymentsPaymentsFeeResponse = {
+    paid_by: PaymentsPaymentsPaidBy;
+    total: PaymentsTotal;
+};
+
