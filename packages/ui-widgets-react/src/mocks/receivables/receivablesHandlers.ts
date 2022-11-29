4bd5bb5532f170884f94ee909299445e6a0ed4ba
Andrei Tarasov <andrey.tarasov@monite.com>
2022-11-29 10:29:37 +0200

    fixed rebase issues

--- packages/ui-widgets-react/src/mocks/receivables/receivablesHandlers.ts
+++ packages/ui-widgets-react/src/mocks/receivables/receivablesHandlers.ts
@@ -73,7 +73,7 @@ export const receivableHandlers = [
   ),
 
   rest.get<undefined, {}, ReceivablesVatRateListResponse>(
-    '*/vat_rates?counterpart_id=:counterpartId',
+    '*/vat_rates?counterpart_id=*',
     (_, res, ctx) => {
       return res(ctx.json(vatRatesByCounterpartId));
     }
