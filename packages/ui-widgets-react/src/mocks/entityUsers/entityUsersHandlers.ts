5ccbbb350c2052600bef52c71618fe74d11de984
Andrei Tarasov <andrey.tarasov@monite.com>
2023-01-19 09:57:32 +0200

    request user info for every row

--- packages/ui-widgets-react/src/mocks/entityUsers/entityUsersHandlers.ts
+++ packages/ui-widgets-react/src/mocks/entityUsers/entityUsersHandlers.ts
@@ -4,6 +4,7 @@ import {
   EntityUserPaginationResponse,
 } from '@team-monite/sdk-api';
 import { entityUsersFixture } from './entityUsersFixture';
+import { entityUserByIdFixture } from './entityUserByIdFixture';
 
 export const entityUsersHandlers = [
   rest.get<undefined, {}, EntityUserPaginationResponse>(
@@ -11,6 +12,15 @@ export const entityUsersHandlers = [
     (_, res, ctx) =>
       res(
         ctx.json(entityUsersFixture as unknown as EntityUserPaginationResponse)
+      )
+  ),
+  rest.get<string, {}, EntityUserPaginationResponse>(
+    `${ENTITY_USERS_ENDPOINT}/:entityUserId`,
+    (_, res, ctx) =>
+      res(
+        ctx.json(
+          entityUserByIdFixture as unknown as EntityUserPaginationResponse
+        )
       )
   ),
 ];
