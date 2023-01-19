ff74123b2acc549f67002ccc53bb7518f577c2c4
Julia Ayubova <juliaayubova@Alvis-MacBook-Pro.local>
2023-01-19 18:41:59 +0300

    fix: confirm stripe only after call calculate_fee

--- packages/ui-widgets-react/src/components/payment/StripeWidget/CheckoutForm/CheckoutForm.tsx
+++ packages/ui-widgets-react/src/components/payment/StripeWidget/CheckoutForm/CheckoutForm.tsx
@@ -71,37 +71,42 @@ export default function CheckoutForm({ paymentIntent }: CheckoutFormProps) {
     setIsLoading(true);
 
     // extra call because of this https://gemms.atlassian.net/browse/DEV-4711
-    monite.api.payment.getFeeByPaymentMethod(id, {
-      payment_method: paymentMethod,
-    });
-
-    try {
-      const { error } = await stripe.confirmPayment({
-        elements,
-        confirmParams: {
-          return_url:
-            `${window.location.origin}/result?data=${rawPaymentData}&payment_method=${paymentMethod}` ||
-            `${window.location.href}`,
-        },
-      });
-      if (error) {
-        // This point will only be reached if there is an immediate error when
-        // confirming the payment. Otherwise, your customer will be redirected to
-        // your `return_url`. For some payment methods like iDEAL, your customer will
-        // be redirected to an intermediate site first to authorize the payment, then
-        // redirected to the `return_url`.
-        if (
-          error.type === 'card_error' ||
-          error.type === 'validation_error' ||
-          error.type === 'invalid_request_error'
-        ) {
-          setMessage(error.message || '');
-        } else {
-          setMessage('An unexpected error occurred.');
-        }
+    const calculateFeeResponse = await monite.api.payment.getFeeByPaymentMethod(
+      id,
+      {
+        payment_method: paymentMethod,
       }
-    } catch (e) {
-      setMessage('An unexpected error occurred.');
+    );
+
+    if (calculateFeeResponse) {
+      try {
+        const { error } = await stripe.confirmPayment({
+          elements,
+          confirmParams: {
+            return_url:
+              `${window.location.origin}/result?data=${rawPaymentData}&payment_method=${paymentMethod}` ||
+              `${window.location.href}`,
+          },
+        });
+        if (error) {
+          // This point will only be reached if there is an immediate error when
+          // confirming the payment. Otherwise, your customer will be redirected to
+          // your `return_url`. For some payment methods like iDEAL, your customer will
+          // be redirected to an intermediate site first to authorize the payment, then
+          // redirected to the `return_url`.
+          if (
+            error.type === 'card_error' ||
+            error.type === 'validation_error' ||
+            error.type === 'invalid_request_error'
+          ) {
+            setMessage(error.message || '');
+          } else {
+            setMessage('An unexpected error occurred.');
+          }
+        }
+      } catch (e) {
+        setMessage('An unexpected error occurred.');
+      }
     }
 
     setIsLoading(false);
