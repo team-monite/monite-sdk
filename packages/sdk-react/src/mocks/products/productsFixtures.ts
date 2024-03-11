eba4783670a2cb427e733f4fe46aa3a489eb1b16
Artem Anashev <artem.anashev@monite.com>
2024-03-11 16:00:15 +0500

    fix(DEV-9738): Update tests

--- packages/sdk-react/src/mocks/products/productsFixtures.ts
+++ packages/sdk-react/src/mocks/products/productsFixtures.ts
@@ -24,9 +24,7 @@ export const productsListFixture: Array<ProductServiceResponse> = new Array(130)
         ? faker.commerce.productDescription()
         : undefined,
       measure_unit_id: getRandomItemFromArray(measureUnitsListFixture.data).id,
-      smallest_amount: faker.datatype.boolean()
-        ? Number(faker.commerce.price({ min: 1, max: 10 }))
-        : undefined,
+      smallest_amount: Number(faker.commerce.price({ min: 1, max: 10 })),
       entity_id: entityIds[0],
       entity_user_id: getRandomProperty(entityUsers).id,
       created_at: faker.date.past().toString(),
