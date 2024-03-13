1561858961948ca0c9385704223778f1c21245c7
Alex Batalov <radist2s@gmail.com>
2024-03-14 03:48:13 +0400

    fix(DEV-9443): `loadedPermissionsValidator(..)` test

--- packages/sdk-react/src/utils/test-utils.test.tsx
+++ packages/sdk-react/src/utils/test-utils.test.tsx
@@ -1,13 +1,13 @@
 import { ENTITY_USERS_QUERY_ID } from '@/core/queries';
-import { ensurePermissionQueriesLoaded } from '@/utils/test-utils';
+import { checkPermissionQueriesLoaded } from '@/utils/test-utils';
 import { QueryClient } from '@tanstack/react-query';
 
 describe('Test Utils', () => {
-  describe('# ensurePermissionQueriesLoaded(..)', () => {
+  describe('# checkPermissionQueriesLoaded(..)', () => {
     test('throw the error if QueryClient is empty', async () => {
       await expect(
-        ensurePermissionQueriesLoaded(new QueryClient())
-      ).rejects.toThrow('Permissions not loaded');
+        checkPermissionQueriesLoaded(new QueryClient())
+      ).rejects.toThrow('Permissions query not exists');
     });
 
     test('resolve "true" if query state "success" for both of queries', async () => {
@@ -22,7 +22,7 @@ describe('Test Utils', () => {
       });
 
       await expect(
-        ensurePermissionQueriesLoaded(queryClient)
+        checkPermissionQueriesLoaded(queryClient)
       ).resolves.toBeUndefined();
     });
 
@@ -34,8 +34,8 @@ describe('Test Utils', () => {
       });
 
       await expect(
-        ensurePermissionQueriesLoaded(new QueryClient())
-      ).rejects.toThrow('Permissions not loaded');
+        checkPermissionQueriesLoaded(new QueryClient())
+      ).rejects.toThrow('Permissions query not exists');
     });
 
     test('throw error if query state of "my_role" query is not "success"', async () => {
@@ -46,8 +46,8 @@ describe('Test Utils', () => {
       });
 
       await expect(
-        ensurePermissionQueriesLoaded(new QueryClient())
-      ).rejects.toThrow('Permissions not loaded');
+        checkPermissionQueriesLoaded(new QueryClient())
+      ).rejects.toThrow('Permissions query not exists');
     });
   });
 });
