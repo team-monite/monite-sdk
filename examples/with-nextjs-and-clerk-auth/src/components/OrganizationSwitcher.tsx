07886c2aba6ccf0c951cc62bb8f4c9eea8a8ec18
Alex Batalov <radist2s@gmail.com>
2024-03-22 03:14:06 +0400

    chore(DEV-9961): add redirection to page root on organization switch

--- examples/with-nextjs-and-clerk-auth/src/components/OrganizationSwitcher.tsx
+++ examples/with-nextjs-and-clerk-auth/src/components/OrganizationSwitcher.tsx
@@ -1,4 +1,9 @@
+'use client';
+
 import { ComponentProps } from 'react';
+
+import { normalizePathTrailingSlash } from 'next/dist/client/normalize-trailing-slash';
+import { usePathname } from 'next/navigation';
 
 import {
   OrganizationSwitcher as OrganizationSwitcherBase,
@@ -10,6 +15,8 @@ export const OrganizationSwitcher = ({
   hidePersonal = true,
   ...restProps
 }: ComponentProps<typeof OrganizationSwitcherBase>) => {
+  const pathname = usePathname();
+
   const { orgRole } = useAuth();
 
   const { userMemberships, setActive } = useOrganizationList({
@@ -21,6 +28,11 @@ export const OrganizationSwitcher = ({
     <OrganizationSwitcherBase
       hidePersonal={hidePersonal}
       afterCreateOrganizationUrl="/counterparts/?display_demo_data_generation_progress"
+      afterSelectOrganizationUrl={() => {
+        // Redirect to the first part of the pathname
+        const pagePathname = pathname.split('/')[1] ?? '';
+        return normalizePathTrailingSlash(`/${pagePathname}`);
+      }}
       {...restProps}
     />
   ) : null;
