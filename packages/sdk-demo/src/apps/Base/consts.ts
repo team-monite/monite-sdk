8b1343511d809ae77d601f3c035d4ea3f4352b6c
Andrei Tarasov <andrey.tarasov@monite.com>
2024-03-25 12:16:53 +0400

    feature(DEV-10099): add Roles menu item to demo

--- packages/sdk-demo/src/apps/Base/consts.ts
+++ packages/sdk-demo/src/apps/Base/consts.ts
@@ -3,6 +3,7 @@ export const ROUTES = {
   payables: '/payables',
   settings: '/settings',
   approvalPolicies: '/settings/approval-policies',
+  roles: '/settings/roles',
   tags: '/settings/tags',
   receivables: '/receivables',
   products: '/products',
