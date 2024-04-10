b6c173803503e96d0cb24985af5caedb3641f461
hypsteria <nep.lubov.alex@gmail.com>
2024-04-10 09:39:25 +0100

    feature(DEV-10078): move eslint plugin from sdkapp to @team-monite/eslint-plugin

--- packages/eslint-plugin/src/config/react.js
+++ packages/eslint-plugin/src/config/react.js
@@ -1,6 +1,10 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ['plugin:@team-monite/vanilla', 'plugin:react-hooks/recommended'],
+  extends: [
+    'plugin:@team-monite/vanilla',
+    'plugin:react-hooks/recommended',
+    'plugin:@tanstack/eslint-plugin-query/recommended',
+  ],
   plugins: ['react-refresh'],
   rules: {
     'react-hooks/exhaustive-deps': 'error',
