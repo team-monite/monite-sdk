4a2adb1ec3d691e70c12d184ddc74a9846176871
Alex Batalov <radist2s@gmail.com>
2024-05-01 08:12:51 +0400

    feature(DEV-10629): add usage of `<QueryClientProvider/>` and `<I18nProvider/>` specific to SDK Demo

--- packages/sdk-demo/src/apps/SDKDemo/SDKDemo.tsx
+++ packages/sdk-demo/src/apps/SDKDemo/SDKDemo.tsx
@@ -1,4 +1,4 @@
-import React, { StrictMode, Suspense } from 'react';
+import React, { StrictMode, Suspense, useMemo } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import { Base } from '@/apps/Base';
@@ -12,27 +12,34 @@ import { LoginForm } from '@/components/LoginForm';
 import { ConfigProvider, useConfig } from '@/context/ConfigContext';
 import { fetchToken } from '@/core/fetchToken';
 import { getThemeConfig, useThemeConfig } from '@/hooks/useThemeConfig.tsx';
+import { messages as defaultMessages } from '@/locales/en/messages.ts';
 import { Global } from '@emotion/react';
+import { setupI18n } from '@lingui/core';
 import { t } from '@lingui/macro';
-import { useLingui } from '@lingui/react';
+import { I18nProvider, useLingui } from '@lingui/react';
 import { useMoniteContext } from '@monite/sdk-react';
 import { Button, createTheme, CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { getFontFaceStyles } from './fontStyles.ts';
 
 export const SDKDemo = () => {
+  const queryClient = useMemo(() => new QueryClient(), []);
+
   return (
     <StrictMode>
       <Suspense>
-        <AuthCredentialsProvider>
-          {(authProps) => (
-            <ConfigProvider>
-              <SDKDemoComponent {...authProps} />
-            </ConfigProvider>
-          )}
-        </AuthCredentialsProvider>
+        <QueryClientProvider client={queryClient}>
+          <AuthCredentialsProvider>
+            {(authProps) => (
+              <ConfigProvider>
+                <SDKDemoComponent {...authProps} />
+              </ConfigProvider>
+            )}
+          </AuthCredentialsProvider>
+        </QueryClientProvider>
       </Suspense>
     </StrictMode>
   );
@@ -46,38 +53,58 @@ const SDKDemoComponent = ({
   const { api_url } = useConfig();
   const { themeConfig, setThemeConfig } = useThemeConfig();
   const apiUrl = `${api_url}/v1`;
-  const muiTheme = createTheme(getThemeConfig(themeConfig));
+  const sdkDemoTheme = createTheme(getThemeConfig(themeConfig));
+  const [sdkDemoLocale, sdkDemoI18n] = useMemo(() => {
+    const localeCode = 'en-US';
+    return [
+      {
+        code: localeCode,
+        messages: {
+          defaultMessages,
+        },
+      },
+      setupI18n({
+        locale: localeCode,
+        messages: {
+          [localeCode]: defaultMessages,
+        },
+      }),
+    ];
+  }, []);
 
   return (
-    <ThemeProvider theme={muiTheme}>
-      <CssBaseline enableColorScheme />
-      <AppMoniteProvider
-        theme={muiTheme}
-        sdkConfig={{
-          entityId: authData?.entity_id ?? 'lazy',
-          apiUrl,
-          fetchToken: () =>
-            authData
-              ? fetchToken(apiUrl, authData).catch(logout)
-              : Promise.reject(),
-        }}
-      >
-        <MoniteReactQueryDevtools />
-        <Global styles={getFontFaceStyles} />
-        {authData ? (
-          <BrowserRouter>
-            <DefaultLayout
-              themeConfig={themeConfig}
-              setThemeConfig={setThemeConfig}
-              siderProps={{ footer: <SiderFooter onLogout={logout} /> }}
-            >
-              <Base />
-            </DefaultLayout>
-          </BrowserRouter>
-        ) : (
-          <LoginForm login={login} />
-        )}
-      </AppMoniteProvider>
+    <ThemeProvider theme={sdkDemoTheme}>
+      <I18nProvider i18n={sdkDemoI18n}>
+        <CssBaseline enableColorScheme />
+        <AppMoniteProvider
+          locale={sdkDemoLocale}
+          theme={sdkDemoTheme}
+          sdkConfig={{
+            entityId: authData?.entity_id ?? 'lazy',
+            apiUrl,
+            fetchToken: () =>
+              authData
+                ? fetchToken(apiUrl, authData).catch(logout)
+                : Promise.reject(),
+          }}
+        >
+          <MoniteReactQueryDevtools />
+          <Global styles={getFontFaceStyles} />
+          {authData ? (
+            <BrowserRouter>
+              <DefaultLayout
+                themeConfig={themeConfig}
+                setThemeConfig={setThemeConfig}
+                siderProps={{ footer: <SiderFooter onLogout={logout} /> }}
+              >
+                <Base />
+              </DefaultLayout>
+            </BrowserRouter>
+          ) : (
+            <LoginForm login={login} />
+          )}
+        </AppMoniteProvider>
+      </I18nProvider>
     </ThemeProvider>
   );
 };
