4e20c5fa873b025f295099bbbaf85a7d87302a06
Alex Batalov <radist2s@gmail.com>
2024-05-04 04:13:45 +0400

    feature(DEV-10395): add usage of `<InvoiceStatusChip/>` to `<InvoicesTables/>`

--- packages/sdk-react/src/components/receivables/InvoicesTable/InvoicesTable.tsx
+++ packages/sdk-react/src/components/receivables/InvoicesTable/InvoicesTable.tsx
@@ -7,7 +7,7 @@ import {
   FILTER_TYPE_SEARCH,
   FILTER_TYPE_STATUS,
 } from '@/components/receivables/consts';
-import { getCommonStatusLabel } from '@/components/receivables/getCommonStatusLabel';
+import { InvoiceStatusChip } from '@/components/receivables/InvoicesTable/InvoiceStatusChip';
 import { PAGE_LIMIT } from '@/constants';
 import { MoniteScopedProviders } from '@/core/context/MoniteScopedProviders';
 import { useCurrencies } from '@/core/hooks/useCurrencies';
@@ -197,14 +197,7 @@ const InvoicesTableBase = ({ onRowClick }: InvoicesTableProps) => {
                 params: GridRenderCellParams<ReceivableResponse>
               ) => {
                 const status = params.value as ReceivablesStatusEnum;
-
-                return (
-                  <Chip
-                    color={ROW_TO_TAG_STATUS_MUI_MAP[status]}
-                    label={getCommonStatusLabel(status, i18n)}
-                    variant="filled"
-                  />
-                );
+                return <InvoiceStatusChip status={status} />;
               },
               flex: 1,
             },
