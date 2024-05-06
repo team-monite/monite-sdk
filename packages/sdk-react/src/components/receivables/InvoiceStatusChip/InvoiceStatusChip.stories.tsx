2c7ad8b172ff5bfd137bfbd07156cbf89de14ab7
Alex Batalov <radist2s@gmail.com>
2024-05-06 16:34:26 +0400

    feature(DEV-10395): improve `<InvoiceStatusChip/>` story

--- packages/sdk-react/src/components/receivables/InvoiceStatusChip/InvoiceStatusChip.stories.tsx
+++ packages/sdk-react/src/components/receivables/InvoiceStatusChip/InvoiceStatusChip.stories.tsx
@@ -1,22 +1,54 @@
+import { ExtendThemeProvider } from '@/utils/storybook-utils';
 import { ReceivablesStatusEnum } from '@monite/sdk-api';
+import { Alert } from '@mui/material';
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { InvoiceStatusChip } from './InvoiceStatusChip';
+import { InvoiceStatusChip as InvoiceStatusChipComponent } from './InvoiceStatusChip';
 
-const meta: Meta<typeof InvoiceStatusChip> = {
-  title: 'Receivables / InvoiceStatusChip',
-  component: InvoiceStatusChip,
+const meta: Meta<typeof InvoiceStatusChipComponent> = {
+  title: 'Components / InvoiceStatusChip',
+  component: InvoiceStatusChipComponent,
 };
 
-type Story = StoryObj<typeof InvoiceStatusChip>;
+type Story = StoryObj<typeof InvoiceStatusChipComponent>;
 
-export const Basic: Story = {
+export const InvoiceStatusChip: Story = {
   args: {
     status: ReceivablesStatusEnum.PAID,
   },
   render: (args) => (
     <div style={{ height: 500, padding: 20 }}>
-      <InvoiceStatusChip {...args} />
+      <ExtendThemeProvider
+        theme={{
+          components: {
+            MoniteInvoiceStatusChip: {
+              defaultProps: {
+                icon: true,
+              },
+              variants: [
+                {
+                  props: { status: ReceivablesStatusEnum.PAID },
+                  style: {
+                    border: '2px dashed lightgreen',
+                  },
+                },
+                {
+                  props: { status: ReceivablesStatusEnum.OVERDUE },
+                  style: {
+                    border: '2px dashed red',
+                  },
+                },
+              ],
+            },
+          },
+        }}
+      >
+        <InvoiceStatusChipComponent {...args} />
+      </ExtendThemeProvider>
+      <Alert sx={{ mt: 2 }}>
+        <code>{'<InvoiceStatusChip />'}</code> could be customized through MUI
+        theming
+      </Alert>
     </div>
   ),
 };
