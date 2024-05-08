5b2112c525e9bb1c29bc773dc857acce1f73c4d2
Alex Batalov <radist2s@gmail.com>
2024-05-09 03:17:21 +0400

    refactor(DEV-10393): move `<ExtendThemeProvider/>` to separate file

---
+++ packages/sdk-react/src/utils/ExtendThemeProvider.tsx
@@ -0,0 +1,28 @@
+import React, { ReactNode } from 'react';
+
+import {
+  createTheme,
+  ThemeOptions,
+  ThemeProvider,
+  useTheme,
+} from '@mui/material';
+
+/**
+ * Extends the current theme with the provided theme options.
+ * @param theme The theme options to extend the current theme with.
+ * @param children
+ */
+export function ExtendThemeProvider({
+  theme,
+  children,
+}: {
+  theme: ThemeOptions;
+  children: ReactNode;
+}) {
+  const mainTheme = useTheme();
+  return (
+    <ThemeProvider theme={createTheme(mainTheme, theme)}>
+      {children}
+    </ThemeProvider>
+  );
+}
