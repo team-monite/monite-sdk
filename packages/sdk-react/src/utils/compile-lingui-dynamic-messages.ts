535ee779e4fb1007635710a944abb6229a902379
Alex Batalov <radist2s@gmail.com>
2024-05-21 04:33:32 +0400

    feature(DEV-10391): use Lingui `generateMessageId(...)` since added [browser support](https://github.com/lingui/js-lingui/releases/tag/v4.6.0)

--- packages/sdk-react/src/utils/compile-lingui-dynamic-messages.ts
+++ packages/sdk-react/src/utils/compile-lingui-dynamic-messages.ts
@@ -2,6 +2,9 @@ import { Messages } from '@lingui/core';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore-next-line
 import { compileMessage } from '@lingui/message-utils/compileMessage';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore-next-line
+import { generateMessageId } from '@lingui/message-utils/generateMessageId';
 
 export async function compileLinguiDynamicMessages<
   T extends { context: string; message: string } | string | object
@@ -52,33 +55,6 @@ const isMessageWithContext = (
   );
 };
 
-const UNIT_SEPARATOR = '\u001F';
-
 export async function generateLinguiMessageId(messageId: string, context = '') {
-  const encoder = new TextEncoder();
-  const data = encoder.encode(messageId + UNIT_SEPARATOR + (context || ''));
-  const hashBuffer = await crypto.subtle.digest('SHA-256', data);
-  const hashArray = Array.from(new Uint8Array(hashBuffer));
-  return toBase64(hashArray).slice(0, 6);
-}
-
-export function toBase64(input: number[]) {
-  const characters =
-    // eslint-disable-next-line lingui/no-unlocalized-strings
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
-  let result = '';
-  let pad = 0;
-  let current = 0;
-
-  for (let i = 0; i < input.length; i += 3) {
-    current = (input[i] << 16) | (input[i + 1] << 8) | input[i + 2];
-    pad = i + 1 < input.length ? (i + 2 < input.length ? 0 : 1) : 2;
-
-    result += characters[current >> 18];
-    result += characters[(current >> 12) & 63];
-    result += pad < 2 ? characters[(current >> 6) & 63] : '=';
-    result += pad < 1 ? characters[current & 63] : '=';
-  }
-
-  return result;
+  return generateMessageId(messageId, context);
 }
