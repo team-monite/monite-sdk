8d81d1c7ae4e5049a03cb164f309478b1d34dbd0
Alex Batalov <radist2s@gmail.com>
2024-05-23 01:51:03 +0400

    chore(DEV-10391): improve `isInvoiceMenuActionAllowed(...)` tests

--- packages/sdk-react/src/components/receivables/InvoicesTable/useInvoiceRowActionMenuCell.test.tsx
+++ packages/sdk-react/src/components/receivables/InvoicesTable/useInvoiceRowActionMenuCell.test.tsx
@@ -10,7 +10,7 @@ import {
   InvoicesTableRowAction,
 } from './useInvoiceRowActionMenuCell';
 
-describe('isInvoiceMenuActionAllowed', () => {
+describe('isInvoiceMenuActionAllowed(...)', () => {
   const menuItems = Object.keys({
     view: true,
     edit: true,
@@ -28,7 +28,7 @@ describe('isInvoiceMenuActionAllowed', () => {
     [key in InvoicesTableRowAction]: true;
   }) as InvoicesTableRowAction[];
 
-  const permissions = [
+  const fullPermissions = [
     {
       action_name: ActionEnum.CREATE,
       permission: PermissionEnum.ALLOWED,
@@ -50,7 +50,7 @@ describe('isInvoiceMenuActionAllowed', () => {
   test('allows each menu item if has permissions', () => {
     expect(
       filterInvoiceActionMenuAllowedItems(
-        permissions,
+        fullPermissions,
         menuItems,
         {
           ...receivableListFixture.invoice[0],
@@ -62,10 +62,106 @@ describe('isInvoiceMenuActionAllowed', () => {
     ).toEqual(menuItems);
   });
 
+  test('disallows each menu item if has permissions', () => {
+    expect(
+      filterInvoiceActionMenuAllowedItems(
+        [
+          {
+            action_name: ActionEnum.CREATE,
+            permission: PermissionEnum.NOT_ALLOWED,
+          },
+          {
+            action_name: ActionEnum.READ,
+            permission: PermissionEnum.NOT_ALLOWED,
+          },
+          {
+            action_name: ActionEnum.UPDATE,
+            permission: PermissionEnum.NOT_ALLOWED,
+          },
+          {
+            action_name: ActionEnum.DELETE,
+            permission: PermissionEnum.NOT_ALLOWED,
+          },
+        ],
+        menuItems,
+        {
+          ...receivableListFixture.invoice[0],
+          status: ReceivablesStatusEnum.DRAFT,
+          entity_user_id: 'user_id_1',
+        },
+        'user_id_1'
+      )
+    ).toEqual([]);
+  });
+
+  test('allows each menu item if invoice belongs to a entity user', () => {
+    expect(
+      filterInvoiceActionMenuAllowedItems(
+        [
+          {
+            action_name: ActionEnum.CREATE,
+            permission: PermissionEnum.ALLOWED_FOR_OWN,
+          },
+          {
+            action_name: ActionEnum.READ,
+            permission: PermissionEnum.ALLOWED_FOR_OWN,
+          },
+          {
+            action_name: ActionEnum.UPDATE,
+            permission: PermissionEnum.ALLOWED_FOR_OWN,
+          },
+          {
+            action_name: ActionEnum.DELETE,
+            permission: PermissionEnum.ALLOWED_FOR_OWN,
+          },
+        ],
+        menuItems,
+        {
+          ...receivableListFixture.invoice[0],
+          status: ReceivablesStatusEnum.DRAFT,
+          entity_user_id: 'user_id_1',
+        },
+        'user_id_1'
+      )
+    ).toEqual(menuItems);
+  });
+
+  test('disallows each menu item if invoice not belongs to a entity user', () => {
+    expect(
+      filterInvoiceActionMenuAllowedItems(
+        [
+          {
+            action_name: ActionEnum.CREATE,
+            permission: PermissionEnum.ALLOWED_FOR_OWN,
+          },
+          {
+            action_name: ActionEnum.READ,
+            permission: PermissionEnum.ALLOWED_FOR_OWN,
+          },
+          {
+            action_name: ActionEnum.UPDATE,
+            permission: PermissionEnum.ALLOWED_FOR_OWN,
+          },
+          {
+            action_name: ActionEnum.DELETE,
+            permission: PermissionEnum.ALLOWED_FOR_OWN,
+          },
+        ],
+        menuItems,
+        {
+          ...receivableListFixture.invoice[0],
+          status: ReceivablesStatusEnum.DRAFT,
+          entity_user_id: 'user_id_1',
+        },
+        'user_id_2'
+      )
+    ).toEqual([]);
+  });
+
   test('filters out unsupported menu items', () => {
     expect(
       filterInvoiceActionMenuAllowedItems(
-        permissions,
+        fullPermissions,
         // @ts-expect-error - testing invalid value
         [...menuItems, 'customAction'],
         {
