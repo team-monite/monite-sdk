e9144dad638b9dd4473007d1ec530fb978243412
Alex Batalov <radist2s@gmail.com>
2024-06-11 14:57:33 +0400

    chore: add `<TestComp />` exported component

--- examples/with-nextjs-and-clerk-auth/src/components/Layout.tsx
+++ examples/with-nextjs-and-clerk-auth/src/components/Layout.tsx
@@ -4,6 +4,7 @@ import React, { ReactNode } from 'react';
 
 import { css, useTheme } from '@emotion/react';
 import { Box, Drawer } from '@mui/material';
+import { TestComp } from '@team-monite/sdk-demo';
 
 import { NavigationMenu } from '@/components/NavigationMenu';
 import { OrganizationSwitcher } from '@/components/OrganizationSwitcher';
@@ -14,47 +15,7 @@ export const Layout = ({ children }: { children: ReactNode }) => {
 
   return (
     <Box sx={{ display: 'flex' }}>
-      <Drawer
-        PaperProps={{ sx: { backgroundColor: 'paper' } }}
-        sx={{
-          width: '240px',
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: '240px',
-            boxSizing: 'border-box',
-          },
-        }}
-        variant="permanent"
-        anchor="left"
-      >
-        <div
-          css={css`
-            display: flex;
-            padding: ${theme.spacing(2)};
-            gap: ${theme.spacing(2)};
-            align-items: flex-start;
-            box-sizing: border-box;
-            z-index: 10;
-          `}
-        >
-          <UserButton />
-          <OrganizationSwitcher />
-        </div>
-
-        <NavigationMenu />
-      </Drawer>
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          bgcolor: 'background.default',
-          px: 3,
-          py: 1,
-          minWidth: 0,
-        }}
-      >
-        {children}
-      </Box>
+      <TestComp />
     </Box>
   );
 };
---
+++ packages/sdk-demo/src/TestComp.tsx
@@ -0,0 +1,5 @@
+import { Check } from '@mui/icons-material';
+
+export function TestComp() {
+  return <>{<Check />} Hello, World!</>;
+}
