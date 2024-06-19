1ded9920c8eae423465c9f6c0e705c853ce5add7
Andrei Tarasov <tarasov.a.dev@gmail.com>
2024-06-19 20:56:55 +0400

    feat(DEV-11087): add super component

---
+++ packages/sdk-react/src/components/approvalRequests/ApprovalRequests.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import { PageHeader, ProductsTable } from '@/components';
+import { ApprovalRequestsTable } from '@/components/approvalRequests/ApprovalRequestsTable';
+import { MoniteScopedProviders } from '@/core/context/MoniteScopedProviders';
+import { useEntityUserByAuthToken } from '@/core/queries';
+import { useIsActionAllowed } from '@/core/queries/usePermissions';
+import { AccessRestriction } from '@/ui/accessRestriction';
+import { ActionEnum } from '@/utils/types';
+import { t } from '@lingui/macro';
+import { useLingui } from '@lingui/react';
+import { CircularProgress } from '@mui/material';
+
+export const ApprovalRequests = () => (
+  <MoniteScopedProviders>
+    <ApprovalRequestsBase />
+  </MoniteScopedProviders>
+);
+
+const ApprovalRequestsBase = () => {
+  const { i18n } = useLingui();
+
+  const { data: user } = useEntityUserByAuthToken();
+
+  const { data: isReadAllowed, isLoading: isReadAllowedLoading } =
+    useIsActionAllowed({
+      method: 'approval_request',
+      action: ActionEnum.READ,
+      entityUserId: user?.id,
+    });
+
+  return (
+    <>
+      <PageHeader
+        title={
+          <>
+            {t(i18n)`Approval Requests`}
+            {isReadAllowedLoading && (
+              <CircularProgress size="0.7em" color="secondary" sx={{ ml: 1 }} />
+            )}
+          </>
+        }
+      />
+      {!isReadAllowedLoading && !isReadAllowed && <AccessRestriction />}
+      {isReadAllowed && <ApprovalRequestsTable />}
+    </>
+  );
+};
--- packages/sdk-react/src/components/approvalRequests/index.ts
+++ packages/sdk-react/src/components/approvalRequests/index.ts
@@ -1,1 +1,2 @@
+export * from './ApprovalRequests';
 export * from './ApprovalRequestsTable/';
