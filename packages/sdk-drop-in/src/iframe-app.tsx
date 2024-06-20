40ecf16ec568787a508c101ecfe53c5ddcbae80e
Alex Batalov <radist2s@gmail.com>
2024-06-20 04:39:14 +0400

    refactor(DEV-11277): add initial implementation of `monite-iframe-app` and it's consumer application

--- packages/sdk-drop-in/src/iframe-app.tsx
+++ packages/sdk-drop-in/src/iframe-app.tsx
@@ -1,232 +1,106 @@
-import React, {
-  ComponentProps,
-  ElementType,
-  ReactNode,
-  useEffect,
-  useState,
-} from 'react';
-import {
-  BrowserRouter,
-  HashRouter,
-  MemoryRouter,
-  Routes,
-} from 'react-router-dom';
+import React, { ComponentProps, Suspense, useMemo } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import { ConfigLoader } from '@/lib/ConfigLoader.tsx';
+import { EntityIdLoader } from '@/lib/EntityIdLoader.tsx';
+import { moniteSuperComponents } from '@/lib/moniteSuperComponents.tsx';
 import { css, Global } from '@emotion/react';
-import { createAPIClient, RootElementsProvider } from '@monite/sdk-react';
-import { createSecureRequestFn } from '@openapi-qraft/react';
-import { QueryClient } from '@tanstack/react-query';
-import { getConfig } from '@team-monite/sdk-demo';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import { DropInMoniteProvider } from './CommonMoniteProvider.tsx';
+import { DropInMoniteProvider } from './DropInMoniteProvider';
 
-const Payables = React.lazy(() =>
-  import('@monite/sdk-react').then((module) => ({ default: module.Payables }))
-);
-const Receivables = React.lazy(() =>
-  import('@monite/sdk-react').then((module) => ({
-    default: module.Receivables,
-  }))
-);
-const Counterparts = React.lazy(() =>
-  import('@monite/sdk-react').then((module) => ({
-    default: module.Counterparts,
-  }))
-);
-const Products = React.lazy(() =>
-  import('@monite/sdk-react').then((module) => ({ default: module.Products }))
-);
-const Tags = React.lazy(() =>
-  import('@monite/sdk-react').then((module) => ({ default: module.Tags }))
-);
-const ApprovalPolicies = React.lazy(() =>
-  import('@monite/sdk-react').then((module) => ({
-    default: module.ApprovalPolicies,
-  }))
-);
-const Onboarding = React.lazy(() =>
-  import('@monite/sdk-react').then((module) => ({ default: module.Onboarding }))
-);
-
-type ProviderProps = Pick<
-  ComponentProps<typeof DropInMoniteProvider>,
-  'locale' | 'theme'
->;
-
-type IframeAppProps = Pick<
-  ComponentProps<typeof Router>,
-  'router' | 'basename'
-> &
-  ProviderProps;
-
-interface MoniteIframeAppProps extends IframeAppProps {
-  locale?: Record<string, string>;
+interface MoniteIframeAppProps
+  extends Pick<
+    ComponentProps<typeof DropInMoniteProvider>,
+    'locale' | 'theme'
+  > {
   fetchToken: () => Promise<{
     access_token: string;
     token_type: string;
     expires_in: number;
   }>;
-  component?: WidgetType;
-  basename?: string;
-  router?: keyof typeof supportedRouters;
-  rootElements?: ComponentProps<typeof RootElementsProvider>['elements'];
 }
 
-const getEntityId = async (
-  apiUrl: string,
-  fetchToken: () => Promise<{
-    access_token: string;
-    token_type: string;
-    expires_in: number;
-  }>
-) => {
-  const { api, requestFn } = createAPIClient({});
-  const { id: entity_id } = await api.entityUsers.getEntityUsersMe(
-    {
-      parameters: {},
-      baseUrl: apiUrl,
-    },
-    createSecureRequestFn(
-      {
-        async HTTPBearer() {
-          const { access_token } = await fetchToken();
-          return {
-            token: access_token,
-            refreshInterval: Infinity,
-          };
-        },
-      },
-      requestFn,
-      new QueryClient()
-    )
+export const MoniteIframeApp = (props: MoniteIframeAppProps) => {
+  const queryClient = useMemo(() => new QueryClient(), []);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Suspense fallback={null /** add a fallback **/}>
+        <ConfigLoader>
+          {({ apiUrl, appBasename }) => (
+            // Should be replaced with shared `fetchToken` with built-in invalidation
+            // Current implementation fetches two tokens (first for entityId, second for the app)
+            <EntityIdLoader fetchToken={props.fetchToken} apiUrl={apiUrl}>
+              {(entityId) => (
+                <MoniteIframeAppComponent
+                  {...props}
+                  entityId={entityId}
+                  apiUrl={apiUrl}
+                  basename={appBasename}
+                />
+              )}
+            </EntityIdLoader>
+          )}
+        </ConfigLoader>
+      </Suspense>
+    </QueryClientProvider>
   );
-  return entity_id;
 };
 
-export const IframeApp: React.FC<MoniteIframeAppProps> = ({
+export const MoniteIframeAppComponent = ({
   theme,
   locale,
   fetchToken,
-  component,
-  router,
   basename,
-  rootElements,
+  apiUrl,
+  entityId,
+}: MoniteIframeAppProps & {
+  apiUrl: string;
+  entityId: string;
+  basename: string;
 }) => {
-  const [apiUrl, setApiUrl] = useState<string>('');
-  const [entityId, setEntityId] = useState<string>('');
-
-  useEffect(() => {
-    try {
-      const initialize = async () => {
-        const config = await getConfig();
-        setApiUrl(config.api_url);
-        try {
-          const id = await getEntityId(config.api_url, fetchToken);
-          setEntityId(id);
-        } catch (error) {
-          console.error(error);
-          throw new Error('Error while fetching entity id');
-        }
-      };
-      initialize();
-    } catch (error) {
-      console.error(error);
-      throw new Error('Error while fetching config');
-    }
-  }, [fetchToken]);
-
-  if (router && !(router in supportedRouters))
-    throw new Error('Provided router type is not supported');
-
-  if (!fetchToken) throw new Error('Missing required prop `fetchToken`');
-
-  const Widget: ElementType | null =
-    component && component in mapComponentTypeToWidget
-      ? mapComponentTypeToWidget[component]
-      : null;
-
-  if (!Widget) throw new Error('Provided component is not supported');
-
   return (
-    <RootElementsProvider elements={rootElements}>
-      <DropInMoniteProvider
-        locale={locale}
-        theme={theme}
-        sdkConfig={{
-          entityId,
-          fetchToken,
-          apiUrl,
-        }}
-      >
-        <Global
-          styles={css`
-            :root,
-            :host {
-              line-height: 1.5;
-              -webkit-font-smoothing: antialiased;
-              isolation: isolate;
-            }
-
-            *,
-            *::before,
-            *::after {
-              box-sizing: border-box;
-            }
-          `}
-        />
-        <Router router={router} basename={basename}>
-          <Routes>{Widget && <Widget />}</Routes>
-        </Router>
-      </DropInMoniteProvider>
-    </RootElementsProvider>
-  );
-};
-
-const Router = ({
-  router,
-  basename,
-  children,
-}: {
-  router?: keyof typeof supportedRouters;
-  basename?: string;
-  children: ReactNode;
-}) => {
-  const routerType = router ?? 'memory';
-  const RouterComponent = supportedRouters[routerType];
-
-  return (
-    <RouterComponent
-      basename={basename}
-      initialEntries={
-        basename && routerType === 'memory' ? [basename] : undefined
-      }
+    <DropInMoniteProvider
+      locale={locale}
+      theme={theme}
+      sdkConfig={{
+        entityId,
+        fetchToken,
+        apiUrl,
+      }}
     >
-      {children}
-    </RouterComponent>
+      <Global
+        styles={css`
+          :root,
+          :host {
+            line-height: 1.5;
+            -webkit-font-smoothing: antialiased;
+            isolation: isolate;
+          }
+
+          *,
+          *::before,
+          *::after {
+            box-sizing: border-box;
+          }
+        `}
+      />
+      <BrowserRouter basename={basename}>
+        <Routes>
+          {Object.entries(moniteSuperComponents).map(([path, Component]) => (
+            <Route
+              key={path}
+              path={`/${path}`}
+              element={
+                <Suspense fallback={null /** add a fallback **/}>
+                  <Component />
+                </Suspense>
+              }
+            />
+          ))}
+        </Routes>
+      </BrowserRouter>
+    </DropInMoniteProvider>
   );
 };
-
-type WidgetType =
-  | 'payables'
-  | 'receivables'
-  | 'counterparts'
-  | 'products'
-  | 'tags'
-  | 'approval-policies'
-  | 'onboarding';
-
-const mapComponentTypeToWidget: Record<WidgetType, ElementType> = {
-  payables: Payables,
-  receivables: Receivables,
-  counterparts: Counterparts,
-  products: Products,
-  tags: Tags,
-  onboarding: Onboarding,
-  'approval-policies': ApprovalPolicies,
-};
-
-export const supportedRouters = {
-  browser: BrowserRouter,
-  hash: HashRouter,
-  memory: MemoryRouter,
-} as const;
---
+++ packages/sdk-drop-in/src/monite-iframe-app-html.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { MoniteIframeApp } from '@/iframe-app';
+import { getConfig } from '@/lib/ConfigLoader';
+import { createAPIClient } from '@monite/sdk-react';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+const { api, requestFn } = createAPIClient();
+
+const fetchMockedToken = async () => {
+  const config = await getConfig();
+  return await api.auth.postAuthToken(
+    {
+      body: {
+        entity_user_id: config.entity_user_id,
+        client_id: config.client_id,
+        client_secret: config.client_secret,
+        grant_type: 'entity_user',
+      },
+      baseUrl: `${config.api_url}/v1`,
+      parameters: {},
+    },
+    requestFn
+  );
+};
+
+root.render(
+  <React.StrictMode>
+    <MoniteIframeApp fetchToken={fetchMockedToken} />
+  </React.StrictMode>
+);
