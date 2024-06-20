db2b3bc45e87d1c6a01ea1e7b8ec4f1f91fc2824
Vitaly Kuprin <vitas_s@inbox.lv>
2024-06-20 12:10:47 +0200

    feat(DEV-11280): Implement Iframe Communication using MessageChannel

---
+++ packages/sdk-drop-in/src/hooks/useIframeAppSlots.tsx
@@ -0,0 +1,40 @@
+import { useCallback, useEffect, useState } from 'react';
+
+import { IframeAppManager } from '@/lib/iframeManager.tsx';
+
+const iframeAppManager = new IframeAppManager();
+
+export const useMoniteIframeAppSlots = () => {
+  const [theme, setTheme] = useState<string | null>(null);
+  const [locale, setLocale] = useState<string | null>(null);
+
+  useEffect(() => {
+    const handleMessage = (event: MessageEvent) => {
+      if (event.data.type === 'connect') {
+        const port = event.ports[0];
+        port.onmessage = ({ data }) => {
+          if (data.type === 'theme') {
+            setTheme(data.payload);
+          }
+          if (data.type === 'locale') {
+            setLocale(data.payload);
+          }
+        };
+      }
+    };
+
+    window.addEventListener('message', handleMessage);
+    return () => {
+      window.removeEventListener('message', handleMessage);
+    };
+  }, []);
+
+  const fetchToken = useCallback(() => {
+    return new Promise<string>((resolve) => {
+      iframeAppManager.fetchTokenResolve = resolve;
+      window.parent.postMessage({ type: 'fetch-token' }, '*');
+    });
+  }, []);
+
+  return { theme, locale, fetchToken };
+};
