b7d640d4445cb854b097cb55a1b242e069845926
Alex Batalov <radist2s@gmail.com>
2024-06-24 06:58:49 +0400

    fix(DEV-11277): DRAFT

--- packages/sdk-drop-in/src/hooks/useIframeAppSlots.tsx
+++ packages/sdk-drop-in/src/hooks/useIframeAppSlots.tsx
@@ -1,63 +1,82 @@
-import { useCallback, useState, useSyncExternalStore } from 'react';
+import { useCallback, useRef, useSyncExternalStore } from 'react';
 
-import { IframeAppManager } from '@/lib/IframeClassManager';
+import { MoniteIframeAppMessageChannel } from '@/lib/IframeClassManager';
+
+type AccessToken = {
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+};
 
 export const useMoniteIframeAppSlots = () => {
-  window.parent.postMessage({ type: 'request-token' }, '*'); // Request a token when iframe loads
+  const slotHandler = useRef<{
+    ['fetchToken']?: () => Promise<AccessToken>;
+  }>({});
 
-  const [iframeAppManager] = useState(() => new IframeAppManager());
-  const [token, setToken] = useState<{
-    access_token: string;
-    token_type: string;
-    expires_in: number;
-  } | null>(null);
+  const subscribe = useCallback((onStoreChange: () => void) => {
+    const iframeAppManager = new MoniteIframeAppMessageChannel(window);
 
-  const subscribe = (onStoreChange: () => void) => {
-    iframeAppManager.on('fetch-token', () => {
+    const slotState: {
+      ['fetch-token']?:
+        | {
+            resolve: (value: AccessToken) => void;
+            reject: () => void;
+          }
+        | { promise: Promise<AccessToken> };
+    } = {};
+
+    iframeAppManager.on('fetch-token', (data) => {
+      if (!validateToken(data)) return void console.error('Invalid token data');
+
+      const slot = slotState['fetch-token'];
+
+      if (slot && 'resolve' in slot) {
+        slot?.resolve(data);
+      } else {
+        slotState['fetch-token'] = { promise: Promise.resolve(data) };
+      }
       onStoreChange();
     });
 
-    iframeAppManager.connectWithRetry();
+    iframeAppManager.connect();
 
-    const handleConnectMessage = (event: MessageEvent) => {
-      iframeAppManager.handleConnectMessage(event);
-
-      if (event.data.type === 'token-response') {
-        setToken(event.data.token);
-      }
+    slotHandler.current = {
+      ['fetchToken']: () =>
+        new Promise<AccessToken>((resolve, reject) => {
+          const slot = slotState['fetch-token'];
+          if (slot && 'promise' in slot) {
+            return void slot.promise.then((data) => {
+              slotState['fetch-token'] = undefined;
+              resolve(data);
+            });
+          }
+          if (slot && 'resolve' in slot) slot.reject();
+          slotState['fetch-token'] = { resolve, reject };
+          iframeAppManager.requestSlot('fetch-token');
+        }),
     };
-
-    window.addEventListener('message', handleConnectMessage);
 
     return () => {
-      window.removeEventListener('message', handleConnectMessage);
+      iframeAppManager.destroy();
     };
-  };
+  }, []);
 
-  const getSnapshot = useCallback(() => {
-    return iframeAppManager.state;
-  }, [iframeAppManager.state]);
-
-  const state = useSyncExternalStore(subscribe, getSnapshot);
-
-  const fetchToken = useCallback(() => {
-    return new Promise<{
-      access_token: string;
-      token_type: string;
-      expires_in: number;
-    }>((resolve) => {
-      iframeAppManager.requestToken();
-
-      const handleTokenResponse = (event: MessageEvent) => {
-        if (event.data.type === 'token-response') {
-          resolve(event.data.token);
-          window.removeEventListener('message', handleTokenResponse);
-        }
-      };
-
-      window.addEventListener('message', handleTokenResponse);
-    });
-  }, [iframeAppManager]);
-
-  return { ...state, fetchToken, token };
+  return useSyncExternalStore(
+    subscribe,
+    () => slotHandler.current as Required<typeof slotHandler.current>
+  );
 };
+
+function validateToken(token: unknown): token is {
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+} {
+  return (
+    !!token &&
+    typeof token === 'object' &&
+    'access_token' in token &&
+    'token_type' in token &&
+    'expires_in' in token
+  );
+}
--- packages/sdk-drop-in/src/lib/IframeClassManager.ts
+++ packages/sdk-drop-in/src/lib/IframeClassManager.ts
@@ -1,109 +1,221 @@
-interface IframeAppState {
-  [key: string]: string | null;
-}
-
-export class IframeAppManager {
-  iframe?: HTMLIFrameElement | null;
-  channel: MessageChannel;
-  state: IframeAppState;
-  port?: MessagePort;
-  listeners: { [key: string]: (payload: string) => void };
-
-  constructor(iframeId?: string) {
-    if (typeof iframeId === 'string') {
-      this.iframe = document.getElementById(iframeId) as HTMLIFrameElement;
-    }
-    this.channel = new MessageChannel();
-    this.state = {};
-    this.listeners = {};
-
-    this.preInit();
-    this.initIframe();
-    this.initAppManager();
-  }
-
-  preInit() {
-    window.parent.postMessage({ type: 'readyToConnect' }, '*');
-  }
-
-  initIframe() {
-    if (this.iframe) {
-      this.iframe.addEventListener('load', () => {
-        this.iframe?.contentWindow?.postMessage({ type: 'connect' }, '*', [
-          this.channel.port2,
-        ]);
-
-        this.channel.port1.onmessage = (event) => {
-          const { type, payload } = event.data;
-          if (payload) this.sendMessageToIframe(type, payload);
-        };
-      });
-    }
-  }
-
-  initAppManager() {
-    window.addEventListener('message', (event) => this.handleMessage(event));
-  }
-
-  handleMessage(event: MessageEvent) {
-    if (event.data.type === 'connect') {
-      this.port = event.ports[0];
-      this.port.onmessage = ({ data }) => {
-        if (data.type in this.listeners) {
-          this.listeners[data.type](data.payload);
-        }
-      };
-    }
-  }
-
-  sendMessageToIframe(type: string, payload: string | undefined) {
-    try {
-      this.channel.port1.postMessage({
-        type,
-        payload: JSON.stringify(payload),
-      });
-    } catch (error) {
-      console.error('Error sending message to iframe:', error);
-    }
-  }
-
-  on(event: string, callback: (payload: string) => void) {
-    this.listeners[event] = (payload: string) => {
-      const deserializedPayload = JSON.parse(payload);
-      callback(deserializedPayload);
+class MessageChannelPortManager {
+  public port?: MessagePort;
+  private listeners: { [event_name: string]: (payload: unknown) => void } = {};
+  private slots: {
+    [slotName: string]: {
+      payload: PayloadSlot;
+      abortController: AbortController | null;
     };
-  }
+  } = {};
 
-  connectWithRetry(retryCount = 0) {
-    if (this.port) {
-      try {
-        this.port.postMessage({ type: 'connect' });
-
-        this.port.onmessage = (event) => {
-          const { type, payload } = event.data;
-          if (type in this.listeners) {
-            this.listeners[type](payload);
-          }
-        };
-      } catch (error) {
-        console.error('Error during port message handling:', error);
-      }
-    } else if (retryCount < 5) {
-      setTimeout(() => this.connectWithRetry(retryCount + 1), 1000);
-    }
-  }
-
-  handleConnectMessage(event: MessageEvent) {
-    if (event.data.type !== 'connect') return;
-
-    this.port!.onmessage = ({ data }) => {
-      if (data.type in this.listeners) {
+  public connect(port: MessagePort) {
+    this.port = port;
+    this.port.onmessage = ({ data }) => {
+      if (data.type in this.slots && typeof data.payload === 'number') {
+        if (!this.port) throw new Error('Port is not set');
+        this.send(data.type);
+      } else if (
+        this.listeners[data.type] &&
+        typeof data.payload !== 'number'
+      ) {
         this.listeners[data.type](data.payload);
       }
     };
+
+    Object.keys(this.slots).forEach((slotName) => {
+      this.send(slotName);
+    });
   }
 
-  requestToken() {
-    window.parent.postMessage({ type: 'fetch-token' }, '*');
+  public disconnect() {
+    this.port?.close();
+    Object.values(this.slots).forEach(
+      ({ abortController }) => void abortController?.abort()
+    );
+  }
+
+  public destroy() {
+    this.disconnect();
+    this.slots = {};
+    this.listeners = {};
+  }
+
+  public slot(slotName: string, data: PayloadSlot) {
+    this.slots[slotName] = {
+      ...this.slots[slotName],
+      payload: data,
+    };
+
+    this.send(slotName);
+  }
+
+  public removeSlot(slotName: string) {
+    this.slots[slotName].abortController?.abort();
+    delete this.slots[slotName];
+  }
+
+  public on(event: string, listener: (payload: unknown) => void) {
+    this.listeners[event] = listener;
+  }
+
+  public off(event: string) {
+    delete this.listeners[event];
+  }
+
+  private send(slotName: string) {
+    const port = this.port;
+
+    if (!port) return;
+
+    this.slots[slotName].abortController?.abort();
+
+    const payloadSlot = this.slots[slotName].payload;
+
+    if (typeof payloadSlot === 'function') {
+      const abortController = new AbortController();
+      this.slots[slotName].abortController = abortController;
+      payloadSlot({ signal: abortController.signal }).then((resultPayload) => {
+        assertSlotPayload(resultPayload);
+        port.postMessage({
+          type: slotName,
+          payload: resultPayload,
+        });
+      });
+    } else {
+      assertSlotPayload(payloadSlot);
+      port.postMessage({
+        type: slotName,
+        payload: payloadSlot,
+      });
+    }
+
+    function assertSlotPayload(
+      rawPayload: unknown
+    ): asserts rawPayload is PayloadSlot {
+      if (typeof rawPayload !== 'undefined') return;
+      throw new Error(
+        `Slot '${slotName}' payload must be a function or a primitive value. Got: ${rawPayload}`
+      );
+    }
   }
 }
+
+export class MoniteIframeAppConsumerMessageChannel {
+  private channel: MessageChannel;
+  private reconnectPollingInterval?: number;
+  private portManager: MessageChannelPortManager;
+
+  constructor(private targetIframe: HTMLIFrameElement) {
+    this.channel = new MessageChannel();
+
+    this.portManager = new MessageChannelPortManager();
+    this.targetIframe.addEventListener('load', this.onTargetIframeLoad);
+  }
+
+  public slot(slotName: string, data: PayloadSlot) {
+    this.portManager.slot(slotName, data);
+  }
+
+  public removeSlot(slotName: string) {
+    this.portManager.removeSlot(slotName);
+  }
+
+  public on(event: string, listener: (payload: unknown) => void) {
+    this.portManager.on(event, listener);
+  }
+
+  public off(event: string) {
+    this.portManager.off(event);
+  }
+
+  public destroy() {
+    clearInterval(this.reconnectPollingInterval);
+    this.targetIframe.removeEventListener('load', this.onTargetIframeLoad);
+    this.portManager.destroy();
+  }
+
+  private onTargetIframeLoad = () => {
+    this.targetIframe.removeEventListener('load', this.onTargetIframeLoad);
+    window.addEventListener('message', this.onReadyMessage);
+  };
+
+  onReadyMessage = (event: MessageEvent) => {
+    if (event.data.type !== 'ready') return;
+    window?.removeEventListener('message', this.onReadyMessage);
+    this.portManager.connect(this.channel.port1);
+    this.targetIframe.contentWindow?.postMessage({ type: 'connect' }, '*', [
+      this.channel.port2,
+    ]);
+  };
+}
+
+export class MoniteIframeAppMessageChannel {
+  private portManager: MessageChannelPortManager;
+  private readyPollingInterval?: number;
+
+  constructor(protected targetWindow: Window) {
+    this.portManager = new MessageChannelPortManager();
+
+    this.targetWindow.addEventListener('message', this.onTargetWindowMessage);
+  }
+
+  private onTargetWindowMessage = (event: MessageEvent) => {
+    const communicationPort = event.ports[0];
+    if (!communicationPort) return;
+
+    if (event.data.type !== 'connect') return;
+    if (this.portManager.port !== communicationPort) {
+      this.portManager.disconnect();
+      this.portManager.connect(communicationPort);
+    }
+  };
+
+  public connect = () => {
+    const ready = () => {
+      this.targetWindow.parent.postMessage({ type: 'ready' }, '*');
+    };
+
+    this.readyPollingInterval = setInterval(
+      () => void ready(),
+      1000
+    ) as unknown as number;
+
+    ready();
+  };
+
+  public requestSlot(slotName: string) {
+    this.portManager.slot(slotName, Date.now());
+  }
+
+  public removeSlot(slotName: string) {
+    this.portManager.removeSlot(slotName);
+  }
+
+  public on(event: string, listener: (payload: unknown) => void) {
+    this.portManager.on(event, listener);
+  }
+
+  public off(event: string) {
+    this.portManager.off(event);
+  }
+
+  public destroy() {
+    this.targetWindow.removeEventListener(
+      'message',
+      this.onTargetWindowMessage
+    );
+    this.portManager.destroy();
+  }
+}
+
+type PayloadSlot =
+  | string
+  | number
+  | boolean
+  | null
+  | undefined
+  | Record<string, unknown>
+  | Array<unknown>
+  | {
+      (props: { signal: AbortSignal }): Promise<unknown>;
+    };
