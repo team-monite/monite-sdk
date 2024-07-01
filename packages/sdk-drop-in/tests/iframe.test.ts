5bd3df5fe0a19725fd739eb352ac984e3759d268
Vitaly Kuprin <vitas_s@inbox.lv>
2024-07-01 10:15:50 +0200

    chore: fix some comments

--- packages/sdk-drop-in/tests/iframe.test.ts
+++ packages/sdk-drop-in/tests/iframe.test.ts
@@ -1,56 +1,10 @@
 import { test } from '@playwright/test';
 
-import * as fs from 'node:fs';
 import * as process from 'node:process';
-
-import { WidgetType } from '../src/apps/MoniteApp';
-
-interface Config {
-  client_id: string;
-  client_secret: string;
-  entity_user_id: string;
-  grant_type: string;
-}
-
-const getFileConfig = (): Config => {
-  try {
-    const config: Config = JSON.parse(
-      fs.readFileSync('./public/config.json', 'utf8')
-    );
-    return { ...config };
-  } catch (error) {
-    console.error('Error reading config.json file', error);
-  }
-};
-
-const geEnvConfig = (): Config => {
-  const entityUserId = process.env.MONITE_E2E_APP_CONFIG_ENTITY_USER_ID;
-  const clientId = process.env.MONITE_E2E_APP_CONFIG_CLIENT_ID;
-  const clientSecret = process.env.MONITE_E2E_APP_CONFIG_CLIENT_SECRET;
-
-  if (!entityUserId || !clientId || !clientSecret) {
-    throw new Error('Missing required environment variables');
-  }
-
-  return {
-    entity_user_id: entityUserId,
-    client_id: clientId,
-    client_secret: clientSecret,
-    grant_type: 'entity_user',
-  };
-};
-
-// console.log('ToDo: check where we store the token', getFileConfig());
-// console.log('ToDo: check where we store the token', geEnvConfig());
-
-console.log(process.env);
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-console.log(import.meta.env || "can't find env");
 
 const consumerPage = '/monite-iframe-app-consumer';
 
-const routingPaths: Record<WidgetType, string> = {
+const routingPaths: Record<string, string> = {
   payables: '/payables',
   receivables: '/receivables',
   counterparts: '/counterparts',
@@ -62,20 +16,18 @@ const routingPaths: Record<WidgetType, string> = {
 
 test.describe('Monite Iframe Integration', () => {
   test.beforeEach(async ({ page }) => {
+    await page.route('/config.json', async (route) => {
+      if (process.env.CI) {
+        await route.fulfill({
+          contentType: 'application/json',
+          body: process.env.MONITE_E2E_APP_ADMIN_CONFIG_JSON,
+        });
+      } else {
+        await route.continue();
+      }
+    });
+
     await page.goto(`${consumerPage}${routingPaths.receivables}`);
-  });
-
-  test('should see the sales and navigate inside the iframe', async ({
-    page,
-  }) => {
-    const iframe = page.frameLocator('iframe');
-
-    await iframe.getByRole('heading', { name: 'Sales' }).click();
-    await iframe.getByRole('tab', { name: 'Invoices' }).click();
-    await iframe.getByRole('tab', { name: 'Quotes' }).click();
-    await iframe.getByRole('tab', { name: 'Credit notes' }).click();
-    await iframe.getByRole('button', { name: 'Create Invoice' }).click();
-    await iframe.getByLabel('close').click();
   });
 
   test('test the theme switcher and tabs', async ({ page }) => {
@@ -89,22 +41,13 @@ test.describe('Monite Iframe Integration', () => {
     await page.getByRole('button', { name: 'Settings' }).click();
     await page.getByRole('button', { name: 'Roles' }).click();
     await page.getByRole('button', { name: 'Tags' }).click();
-
-    const iframe = page.frameLocator('iframe');
-    await iframe.getByRole('button', { name: 'Create new tag' }).click();
-    await iframe.getByRole('button', { name: 'Cancel' }).click();
   });
 
-  type WidgetTest = {
-    path: string;
-    name: string;
-  };
-
-  const widgetTests: WidgetTest[] = [
+  const widgetTests = [
     { path: routingPaths.payables, name: 'Payables' },
     { path: routingPaths.counterparts, name: 'Counterparts' },
     { path: routingPaths.products, name: 'Products' },
-  ];
+  ] as const;
 
   widgetTests.forEach(({ path, name }) => {
     test(`should see the ${name.toLowerCase()} tab`, async ({ page }) => {
