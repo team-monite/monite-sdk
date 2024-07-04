65d1ff6e954ed993877b0c00b84ba05bb1fa0a93
Alex Batalov <radist2s@gmail.com>
2024-07-04 15:28:25 +0400

    refactor: simplify config.json mocking

--- packages/sdk-drop-in/tests/iframe.test.ts
+++ packages/sdk-drop-in/tests/iframe.test.ts
@@ -1,28 +1,7 @@
 import { WidgetType } from '@/apps/MoniteApp';
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
-import { writeFileSync } from 'fs';
-import * as process from 'node:process';
-import * as path from 'path';
-import { fileURLToPath } from 'url';
-
-const mockedConfigData = {
-  stand: 'dev',
-  api_url: 'https://api.dev.monite.com',
-  app_basename: 'monite-iframe-app',
-  app_hostname: '127.0.0.1',
-  entity_user_id: 'mocked_entity_id',
-  client_id: 'mocked_client_id',
-  client_secret: 'mocked_client_secret',
-};
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-if (process.env.CI) {
-  const configFilePath = path.join(__dirname, '../public', 'config.json');
-  writeFileSync(configFilePath, JSON.stringify(mockedConfigData, null, 2));
-}
+import process from 'node:process';
 
 const consumerPage = '/monite-iframe-app-demo';
 
