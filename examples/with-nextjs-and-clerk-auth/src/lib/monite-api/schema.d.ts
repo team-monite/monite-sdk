911ab25e1484a4167003b4b2a8ccdd4a5f71669c
Alex Batalov <radist2s@gmail.com>
2024-07-08 15:44:28 +0400

    chore(DEV-11661): add a specific API usage to `sdk-demo-with-nextjs-and-clerk-auth`

--- examples/with-nextjs-and-clerk-auth/src/lib/monite-api/schema.d.ts
+++
@@ -1,6 +0,0 @@
-import { APISchema } from '@monite/sdk-react';
-
-export type paths = APISchema.paths;
-export type components = APISchema.components;
-export type operations = APISchema.operations;
-export type webhooks = APISchema.webhooks;
