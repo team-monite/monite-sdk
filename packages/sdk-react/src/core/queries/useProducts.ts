9e28c84a30cc8d7d7a0440e752a8165312a59927
Alex Batalov <radist2s@gmail.com>
2024-07-08 18:46:43 +0400

    refactor(DEV-11611): remove usage of `@monite/sdk-api` from ProductsTable
    BREAKING CHANGE: remove `useInfiniteProducts()` hook

--- packages/sdk-react/src/core/queries/useProducts.ts
+++ packages/sdk-react/src/core/queries/useProducts.ts
@@ -13,41 +13,10 @@ import {
 import { useMoniteContext } from '../context/MoniteContext';
 
 const PRODUCT_QUERY_ID = 'product';
-const PRODUCT_INFINITE_QUERY_ID = 'product-infinite';
 
 const productQueryKeys = {
   all: () => [PRODUCT_QUERY_ID],
-  allInfinite: () => [PRODUCT_INFINITE_QUERY_ID],
   detail: (id: string) => [...productQueryKeys.all(), 'detail', id],
-};
-
-export const useInfiniteProducts = (
-  params: ProductsServiceGetAllRequest,
-  options?: {
-    /** Should we enable the request or not? */
-    enabled: boolean;
-  }
-) => {
-  const { monite } = useMoniteContext();
-
-  return useInfiniteQuery<
-    ProductServicePaginationResponse,
-    ApiError,
-    InfiniteData<ProductServicePaginationResponse, unknown>,
-    (string | ProductsServiceGetAllRequest)[],
-    string
-  >({
-    queryKey: [...productQueryKeys.allInfinite(), params],
-    queryFn: ({ pageParam }) => {
-      return monite.api.products.getAll({
-        ...params,
-        paginationToken: pageParam,
-      });
-    },
-    initialPageParam: '',
-    getNextPageParam: (lastPage) => lastPage.next_pagination_token,
-    enabled: options?.enabled ?? true,
-  });
 };
 
 export const useProductById = (id?: string) => {
