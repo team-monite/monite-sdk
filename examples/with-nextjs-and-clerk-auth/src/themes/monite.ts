fdc961059f561d75b5499af25b4ebaf4299b8029
Edward Coleridge Smith <edcoleridgesmith@gmail.com>
2024-07-08 12:08:19 +0100

    chore(DEV-9444): move tab styling into monite sdk theme

--- examples/with-nextjs-and-clerk-auth/src/themes/monite.ts
+++ examples/with-nextjs-and-clerk-auth/src/themes/monite.ts
@@ -1,49 +1,15 @@
 import { createTheme } from '@mui/material';
 import type { Components } from '@mui/material/styles/components';
-import type {
-  Palette,
-  PaletteOptions,
-} from '@mui/material/styles/createPalette';
+import type { Palette } from '@mui/material/styles/createPalette';
 import type { Theme } from '@mui/material/styles/createTheme';
 import type { TypographyOptions } from '@mui/material/styles/createTypography';
 import { deepmerge } from '@mui/utils';
 import {
   moniteLight as baseMoniteLight,
   moniteDark as baseMoniteDark,
+  neutralLight,
+  neutralDark,
 } from '@team-monite/sdk-themes';
-
-const paletteLight: PaletteOptions = {
-  primary: {
-    dark: '#1D59CC',
-    main: '#3737FF',
-    light: '#F4F8FF',
-  },
-  background: {
-    menu: '#F1F2F5',
-    highlight: '#EBEBFF',
-  },
-  neutral: {
-    '10': '#111111',
-    '50': '#707070',
-    '80': '#DDDDDD',
-  },
-};
-
-const paletteDark: PaletteOptions = {
-  primary: {
-    dark: '#1D59CC',
-    main: '#3737FF',
-    light: '#F4F8FF',
-  },
-  background: {
-    menu: '#F1F2F505',
-  },
-  neutral: {
-    '80': '#B8B8B8',
-    '50': '#F3F3F3',
-    '10': '#FFFFFF',
-  },
-};
 
 const typography:
   | TypographyOptions
@@ -60,11 +26,6 @@ const typography:
     fontSize: '0.875rem',
     fontWeight: 500,
   },
-  h2: {
-    fontSize: 32,
-    fontStyle: 'normal',
-    fontWeight: 600,
-  },
   label2: {
     fontSize: '0.875rem',
     fontStyle: 'normal',
@@ -79,44 +40,23 @@ const typography:
 
 const typographyLight = deepmerge(typography, {
   body2: {
-    color: paletteLight.neutral && paletteLight.neutral['10'],
+    color: neutralLight['10'],
   },
   label3: {
-    color: paletteLight.neutral && paletteLight.neutral['50'],
+    color: neutralLight['50'],
   },
 });
 
 const typographyDark = deepmerge(typography, {
   body2: {
-    color: paletteDark.neutral && paletteDark.neutral['10'],
+    color: neutralDark['10'],
   },
   label3: {
-    color: paletteDark.neutral && paletteDark.neutral['80'],
+    color: neutralDark['80'],
   },
 });
 
 const components: Components<Omit<Theme, 'components'>> = {
-  MuiButton: {
-    styleOverrides: {
-      containedPrimary: {
-        padding: '6px 16px',
-        height: 40,
-        boxShadow: 'none',
-        borderRadius: 8,
-        fontSize: 16,
-        fontWeight: 500,
-      },
-
-      root: {
-        '&.ThemeSelect': {
-          borderRadius: 8,
-        },
-        '&.ThemeSelect .ThemeSelect-modeLabel': {
-          display: 'flex',
-        },
-      },
-    },
-  },
   MuiDrawer: {
     styleOverrides: {
       root: {
@@ -166,54 +106,11 @@ const components: Components<Omit<Theme, 'components'>> = {
       },
     },
   },
-  MuiTab: {
-    styleOverrides: {
-      root: {
-        fontSize: 14,
-        marginRight: 24,
-        color: paletteLight.neutral && paletteLight.neutral['50'],
-
-        '&.Mui-selected': {
-          color: paletteLight.neutral && paletteLight.neutral['10'],
-        },
-      },
-    },
-  },
-  MuiTabs: {
-    styleOverrides: {
-      root: {
-        marginBottom: 32,
-      },
-      indicator: {
-        height: 4,
-        backgroundColor: paletteLight.neutral && paletteLight.neutral['50'],
-        borderTopRightRadius: 8,
-        borderTopLeftRadius: 8,
-      },
-    },
-  },
 };
-
-const componentsDark = deepmerge(components, {
-  MuiTab: {
-    styleOverrides: {
-      root: {
-        color: paletteDark.neutral && paletteDark.neutral['80'],
-        '&.Mui-selected': { color: 'primary.main' },
-      },
-    },
-  },
-  MuiTabs: {
-    styleOverrides: {
-      indicator: { backgroundColor: 'primary.main' },
-    },
-  },
-});
 
 export const moniteLight = () =>
   createTheme(
     deepmerge(baseMoniteLight, {
-      palette: paletteLight,
       typography: typographyLight,
       components,
     })
@@ -222,8 +119,7 @@ export const moniteLight = () =>
 export const moniteDark = () =>
   createTheme(
     deepmerge(baseMoniteDark, {
-      palette: paletteDark,
       typography: typographyDark,
-      components: componentsDark,
+      components,
     })
   );
--- packages/sdk-themes/src/index.ts
+++ packages/sdk-themes/src/index.ts
@@ -1,10 +1,12 @@
 export {
-  moniteLight,
-  moniteDark,
-  paletteDark,
-  paletteLight,
   defaultMoniteComponents,
   defaultMoniteTypography,
+  moniteDark,
+  moniteLight,
+  neutralDark,
+  neutralLight,
+  paletteDark,
+  paletteLight,
 } from './themes/monite.js';
 
 export { materialLight, materialDark } from './themes/material.js';
