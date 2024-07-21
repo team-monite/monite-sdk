bb7ee2f6838f032d71c7a4c7f36cd03add25f350
Andrei Tarasov <tarasov.a.dev@gmail.com>
2024-07-21 15:54:25 +0400

    feat(DEV-11876): reuse reminder validation schema

--- packages/sdk-react/src/components/receivables/InvoiceDetails/CreateReceivable/sections/components/CreateBeforeDueDateReminder/CreateBeforeDueDateReminder.tsx
+++ packages/sdk-react/src/components/receivables/InvoiceDetails/CreateReceivable/sections/components/CreateBeforeDueDateReminder/CreateBeforeDueDateReminder.tsx
@@ -23,82 +23,55 @@ import {
   Divider,
   IconButton,
   Stack,
-  Switch,
   Typography,
-  FormControlLabel,
 } from '@mui/material';
 
 import * as yup from 'yup';
 
-const getValidationSchema = (i18n: I18n) =>
-  yup.object({
+const getValidationSchema = (i18n: I18n) => {
+  const getReminderValidationSchema = (value: boolean) => {
+    if (!value) return yup.object().notRequired();
+
+    return yup.object({
+      days_before: yup
+        .number()
+        .label(t(i18n)`Remind`)
+        .min(1)
+        .max(10000)
+        .required(),
+      subject: yup
+        .string()
+        .label(t(i18n)`Subject`)
+        .required(),
+      body: yup
+        .string()
+        .label(t(i18n)`Body`)
+        .required(),
+    });
+  };
+
+  return yup.object({
     name: yup
       .string()
       .label(t(i18n)`Preset Name`)
       .min(1)
       .max(255)
       .required(),
-    term_1_reminder: yup.object().when('is_discount_date_1', (value) => {
-      if (!value) return yup.object().notRequired();
-
-      return yup.object({
-        days_before: yup
-          .number()
-          .label(t(i18n)`Remind`)
-          .min(1)
-          .max(10000)
-          .required(),
-        subject: yup
-          .string()
-          .label(t(i18n)`Subject`)
-          .required(),
-        body: yup
-          .string()
-          .label(t(i18n)`Body`)
-          .required(),
-      });
-    }),
-    term_2_reminder: yup.object().when('is_discount_date_2', (value) => {
-      if (!value) return yup.object().notRequired();
-
-      return yup.object({
-        days_before: yup
-          .number()
-          .label(t(i18n)`Remind`)
-          .min(1)
-          .max(10000)
-          .required(),
-        subject: yup
-          .string()
-          .label(t(i18n)`Subject`)
-          .required(),
-        body: yup
-          .string()
-          .label(t(i18n)`Body`)
-          .required(),
-      });
-    }),
-    term_final_reminder: yup.object().when('is_due_date', (value) => {
-      if (!value) return yup.object().notRequired();
-
-      return yup.object({
-        days_before: yup
-          .number()
-          .label(t(i18n)`Remind`)
-          .min(1)
-          .max(10000)
-          .required(),
-        subject: yup
-          .string()
-          .label(t(i18n)`Subject`)
-          .required(),
-        body: yup
-          .string()
-          .label(t(i18n)`Body`)
-          .required(),
-      });
-    }),
+    term_1_reminder: yup
+      .object()
+      .when('is_discount_date_1', (value) =>
+        getReminderValidationSchema(value)
+      ),
+    term_2_reminder: yup
+      .object()
+      .when('is_discount_date_2', (value) =>
+        getReminderValidationSchema(value)
+      ),
+    term_final_reminder: yup
+      .object()
+      .when('is_due_date', (value) => getReminderValidationSchema(value)),
   });
+};
 
 type PaymentReminder = components['schemas']['PaymentReminder'];
 
