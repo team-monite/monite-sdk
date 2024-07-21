f8f4dd85aac8524713f9c5660c578b72b7ecf782
Andrei Tarasov <tarasov.a.dev@gmail.com>
2024-07-21 15:57:27 +0400

    feat(DEV-11876): move validation to file

--- packages/sdk-react/src/components/receivables/InvoiceDetails/CreateReceivable/sections/components/CreateBeforeDueDateReminder/CreateBeforeDueDateReminder.tsx
+++ packages/sdk-react/src/components/receivables/InvoiceDetails/CreateReceivable/sections/components/CreateBeforeDueDateReminder/CreateBeforeDueDateReminder.tsx
@@ -9,7 +9,6 @@ import { RHFTextField } from '@/components/RHF/RHFTextField';
 import { useMoniteContext } from '@/core/context/MoniteContext';
 import { getAPIErrorMessage } from '@/core/utils/getAPIErrorMessage';
 import { yupResolver } from '@hookform/resolvers/yup';
-import type { I18n } from '@lingui/core';
 import { t } from '@lingui/macro';
 import { useLingui } from '@lingui/react';
 import CloseIcon from '@mui/icons-material/Close';
@@ -26,52 +25,7 @@ import {
   Typography,
 } from '@mui/material';
 
-import * as yup from 'yup';
-
-const getValidationSchema = (i18n: I18n) => {
-  const getReminderValidationSchema = (value: boolean) => {
-    if (!value) return yup.object().notRequired();
-
-    return yup.object({
-      days_before: yup
-        .number()
-        .label(t(i18n)`Remind`)
-        .min(1)
-        .max(10000)
-        .required(),
-      subject: yup
-        .string()
-        .label(t(i18n)`Subject`)
-        .required(),
-      body: yup
-        .string()
-        .label(t(i18n)`Body`)
-        .required(),
-    });
-  };
-
-  return yup.object({
-    name: yup
-      .string()
-      .label(t(i18n)`Preset Name`)
-      .min(1)
-      .max(255)
-      .required(),
-    term_1_reminder: yup
-      .object()
-      .when('is_discount_date_1', (value) =>
-        getReminderValidationSchema(value)
-      ),
-    term_2_reminder: yup
-      .object()
-      .when('is_discount_date_2', (value) =>
-        getReminderValidationSchema(value)
-      ),
-    term_final_reminder: yup
-      .object()
-      .when('is_due_date', (value) => getReminderValidationSchema(value)),
-  });
-};
+import { getValidationSchema } from './validation';
 
 type PaymentReminder = components['schemas']['PaymentReminder'];
 
