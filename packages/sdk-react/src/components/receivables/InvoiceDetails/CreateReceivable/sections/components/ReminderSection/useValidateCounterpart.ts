c28dcf6dd128ee84923dbf424ce94bd841a05263
Vitaly Kuprin <vitas_s@inbox.lv>
2024-07-23 02:23:13 +0300

    chore(DEV-11865): integrate hook with permission logic

--- packages/sdk-react/src/components/receivables/InvoiceDetails/CreateReceivable/sections/components/ReminderSection/useValidateCounterpart.ts
+++ packages/sdk-react/src/components/receivables/InvoiceDetails/CreateReceivable/sections/components/ReminderSection/useValidateCounterpart.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useFormContext } from 'react-hook-form';
 
 import { isIndividualCounterpart } from '@/components/counterparts/helpers';
