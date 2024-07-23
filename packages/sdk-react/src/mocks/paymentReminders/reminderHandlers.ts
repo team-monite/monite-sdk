f95ee510a9c884af00a5c6127eadd703cfc27fc6
Vitaly Kuprin <vitas_s@inbox.lv>
2024-07-23 07:16:03 +0300

    chore(DEV-11865): payment reminders

--- packages/sdk-react/src/mocks/paymentReminders/reminderHandlers.ts
+++ packages/sdk-react/src/mocks/paymentReminders/reminderHandlers.ts
@@ -43,7 +43,7 @@ export const remindersHandlers = [
     { overdue_reminder_id: string },
     undefined,
     paths['/overdue_reminders/{overdue_reminder_id}']['get']['responses']['200']['content']['application/json']
-  >('*/payment_reminders/{payment_reminder_id}', async ({ params }) => {
+  >('*/overdue_reminders/*', async ({ params }) => {
     const overdueReminder = overdueReminderListFixture.find(
       (overdueReminder) => overdueReminder.id === params.overdue_reminder_id
     );
@@ -65,7 +65,7 @@ export const remindersHandlers = [
     { payment_reminder_id: string },
     undefined,
     paths['/payment_reminders/{payment_reminder_id}']['get']['responses']['200']['content']['application/json']
-  >('*/payment_reminders/{payment_reminder_id}', async ({ params }) => {
+  >('*/payment_reminders/*', async ({ params }) => {
     const paymentReminder = paymentReminderListFixture.find(
       (paymentReminder) => paymentReminder.id === params.payment_reminder_id
     );
