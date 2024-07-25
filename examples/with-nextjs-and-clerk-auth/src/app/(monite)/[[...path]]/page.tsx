a08b2ebfcecd75803aae3973db74f3422f04b269
ivannikitin <ivan@nikitin.io>
2024-07-25 10:44:40 +0200

    feat(demo): extract payables to a separate page

--- examples/with-nextjs-and-clerk-auth/src/app/(monite)/[[...path]]/page.tsx
+++ examples/with-nextjs-and-clerk-auth/src/app/(monite)/[[...path]]/page.tsx
@@ -1,11 +1,5 @@
 import { Box } from '@mui/material';
 
-import { Payables } from '@/components/MoniteComponents';
-
-export default async function PayablesPage() {
-  return (
-    <Box className="Monite-AbsoluteContainer Monite-Purchases">
-      <Payables />
-    </Box>
-  );
+export default async function DefaultPage() {
+  return <Box className="Monite-AbsoluteContainer Monite-Dashboard"></Box>;
 }
