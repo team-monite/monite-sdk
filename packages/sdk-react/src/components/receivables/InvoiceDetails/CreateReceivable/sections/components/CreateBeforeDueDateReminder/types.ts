cd6367b4cff8dfe90f3043c45704e6953a060a1c
Andrei Tarasov <tarasov.a.dev@gmail.com>
2024-07-25 12:01:44 +0400

    feat(DEV-11876): replace switchers with button for before due date reminders

--- packages/sdk-react/src/components/receivables/InvoiceDetails/CreateReceivable/sections/components/CreateBeforeDueDateReminder/types.ts
+++
@@ -1,9 +0,0 @@
-import { components } from '@/api';
-
-type PaymentReminder = components['schemas']['PaymentReminder'];
-
-export interface CreateBeforeDueDateReminderFormFields extends PaymentReminder {
-  is_discount_date_1: boolean;
-  is_discount_date_2: boolean;
-  is_due_date: boolean;
-}
