53142380a41e77e36466fcc536b088a69d810471
Andrei Tarasov <tarasov.a.dev@gmail.com>
2024-07-26 17:22:26 +0400

    feat(DEV-11876): add edit feature for overdue reminders

--- packages/sdk-react/src/mocks/overdueReminders/overdueRemindersHandlers.ts
+++ packages/sdk-react/src/mocks/overdueReminders/overdueRemindersHandlers.ts
@@ -1,9 +1,25 @@
 import type { components } from '@/api';
-import { createOverdueReminder } from '@/mocks/overdueReminders/overdueRemindersFixtures';
+import {
+  createOverdueReminder,
+  overdueReminderListFixture,
+} from '@/mocks/overdueReminders/overdueRemindersFixtures';
 
 import { http, HttpResponse, delay } from 'msw';
 
 export const overdueRemindersHandlers = [
+  http.get<
+    { overdueReminderId: string },
+    undefined,
+    | components['schemas']['OverdueReminderResponse']
+    | components['schemas']['ErrorSchemaResponse']
+  >('*/overdue_reminders/:overdueReminderId', async () => {
+    await delay();
+
+    return HttpResponse.json(
+      createOverdueReminder(overdueReminderListFixture[0])
+    );
+  }),
+
   http.post<
     {},
     components['schemas']['OverdueReminderRequest'],
