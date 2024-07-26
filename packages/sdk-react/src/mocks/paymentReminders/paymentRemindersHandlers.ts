d4382953905b2655ce5d180d8629d0a075a33598
Andrei Tarasov <tarasov.a.dev@gmail.com>
2024-07-26 15:05:45 +0400

    feat(DEV-11876): add edit feature to payment reminders

--- packages/sdk-react/src/mocks/paymentReminders/paymentRemindersHandlers.ts
+++ packages/sdk-react/src/mocks/paymentReminders/paymentRemindersHandlers.ts
@@ -1,9 +1,25 @@
 import type { components } from '@/api';
-import { createPaymentReminder } from '@/mocks/paymentReminders/paymentRemindersFixtures';
+import {
+  createPaymentReminder,
+  paymentReminderListFixture,
+} from '@/mocks/paymentReminders/paymentRemindersFixtures';
 
 import { http, HttpResponse, delay } from 'msw';
 
 export const paymentRemindersHandlers = [
+  http.get<
+    { paymentReminderId: string },
+    undefined,
+    | components['schemas']['PaymentReminderResponse']
+    | components['schemas']['ErrorSchemaResponse']
+  >('*/payment_reminders/:paymentReminderId', async ({ request, params }) => {
+    await delay();
+
+    return HttpResponse.json(
+      createPaymentReminder(paymentReminderListFixture[0])
+    );
+  }),
+
   http.post<
     {},
     components['schemas']['PaymentReminder'],
@@ -14,5 +30,28 @@ export const paymentRemindersHandlers = [
     await delay();
 
     return HttpResponse.json(createPaymentReminder(jsonBody));
+  }),
+
+  http.patch<
+    { paymentReminderId: string },
+    components['schemas']['PaymentReminderUpdateRequest'],
+    components['schemas']['PaymentReminderResponse']
+  >('*/payment_reminders/:paymentReminderId', async ({ request, params }) => {
+    const jsonBody = await request.json();
+
+    let updatedPaymentReminder = paymentReminderListFixture.find(
+      (paymentReminder) => paymentReminder.id === params.paymentReminderId
+    );
+
+    if (updatedPaymentReminder) {
+      updatedPaymentReminder = {
+        ...updatedPaymentReminder,
+        ...jsonBody,
+      };
+    }
+
+    await delay();
+
+    return HttpResponse.json(updatedPaymentReminder);
   }),
 ];
