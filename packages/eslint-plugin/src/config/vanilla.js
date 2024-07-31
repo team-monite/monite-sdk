66c025ace5fb7718c3680578764dd6b8d423bc85
Vitaly Kuprin <vitas_s@inbox.lv>
2024-07-31 16:24:56 +0200

    chore: plugin eslint to check for unused imports and vars

--- packages/eslint-plugin/src/config/vanilla.js
+++ packages/eslint-plugin/src/config/vanilla.js
@@ -10,11 +10,21 @@ module.exports = {
     'plugin:prettier/recommended',
     'plugin:import/recommended',
   ],
-  plugins: ['import', 'testing-library', '@team-monite'],
+  plugins: ['import', 'testing-library', '@team-monite', 'unused-imports'],
   rules: {
     'prettier/prettier': 'error',
     'import/no-default-export': 'error',
     'import/no-extraneous-dependencies': ['error', { devDependencies: false }],
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
   },
   overrides: [
     {
