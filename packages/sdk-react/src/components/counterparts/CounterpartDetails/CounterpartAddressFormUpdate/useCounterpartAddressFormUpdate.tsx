6c18fe08508c7ff642f46f77da8e8480775cf468
Vitaly Kuprin <vitas_s@inbox.lv>
2024-08-01 13:13:33 +0200

    chore: keep onCancel interface

--- packages/sdk-react/src/components/counterparts/CounterpartDetails/CounterpartAddressFormUpdate/useCounterpartAddressFormUpdate.tsx
+++ packages/sdk-react/src/components/counterparts/CounterpartDetails/CounterpartAddressFormUpdate/useCounterpartAddressFormUpdate.tsx
@@ -21,15 +21,12 @@ export interface CounterpartAddressFormUpdateProps {
   counterpartId: string;
   addressId: string;
   onUpdate?: (id: string) => void;
-  onCancel?: () => void;
+  onCancel?: () => void | undefined;
 }
 export function useCounterpartAddressFormUpdate({
   counterpartId,
   addressId,
   onUpdate,
-  // ToDo: check why we are not using onCancel here, but passing it as prop from parent component
-  // eslint-disable-next-line
-  onCancel,
 }: CounterpartAddressFormUpdateProps) {
   const { data: address } = useCounterpartAddresses(counterpartId);
 
