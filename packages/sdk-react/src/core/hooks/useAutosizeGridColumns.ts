696a848cae2738fb1ed1fb71af7f10214c4b2ae5
ivannikitin <ivan@nikitin.io>
2024-08-10 09:48:58 +0200

    feat Column width should be preserved when sorting or changing pages.

--- packages/sdk-react/src/core/hooks/useAutosizeGridColumns.ts
+++ packages/sdk-react/src/core/hooks/useAutosizeGridColumns.ts
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import * as ReactDOM from 'react-dom';
 
 import { useGridApiRef } from '@mui/x-data-grid';
@@ -10,23 +10,41 @@ import { useGridApiRef } from '@mui/x-data-grid';
 // DOM access is required to accurately calculate dimensions
 export function useAutosizeGridColumns(rows: any) {
   const gridApiRef = useGridApiRef();
+  const [columnWidths, setColumnWidths] = useState<
+    { field: string; width: number }[]
+  >([]);
 
   useEffect(() => {
-    setTimeout(() => {
-      ReactDOM.flushSync(() => {
-        setTimeout(() => {
-          if (gridApiRef.current?.autosizeColumns) {
-            // noinspection JSIgnoredPromiseFromCall
-            gridApiRef.current?.autosizeColumns({
+    const grid = gridApiRef.current;
+    if (!grid || !grid.autosizeColumns) return;
+    if (columnWidths && columnWidths.length > 0) {
+      for (const column of columnWidths) {
+        grid.setColumnWidth(column.field, column.width ?? 100);
+      }
+    } else {
+      setTimeout(() => {
+        ReactDOM.flushSync(() => {
+          setTimeout(async () => {
+            await grid.autosizeColumns({
               includeHeaders: true,
               includeOutliers: true,
               expand: true,
             });
-          }
-        }, 1);
-      });
-    }, 1);
-  }, [gridApiRef, rows]);
+            if (rows && rows.length > 0) {
+              const columns = grid.getAllColumns();
+              setColumnWidths(
+                columns.map((c) => ({
+                  field: c.field,
+                  width: c.width ?? 100,
+                }))
+              );
+              console.log(columns.map((c) => c.width));
+            }
+          }, 1);
+        });
+      }, 1);
+    }
+  }, [gridApiRef, rows, columnWidths]);
 
   return gridApiRef;
 }
