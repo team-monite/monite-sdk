08d7ec22331645c2e733f1f0ef2da6cf9db3f9e6
Alex Batalov <radist2s@gmail.com>
2024-08-15 07:38:35 +0400

    chore(DEV-12191): add `memo(...)` to `<InvoiceCounterpartName/>`

--- packages/sdk-react/src/components/receivables/InvoiceCounterpartName.tsx
+++ packages/sdk-react/src/components/receivables/InvoiceCounterpartName.tsx
@@ -1,3 +1,5 @@
+import { memo } from 'react';
+
 import { getCounterpartName } from '@/components/counterparts/helpers';
 import { useCounterpartById } from '@/core/queries';
 import { Skeleton } from '@mui/material';
@@ -6,22 +8,22 @@ interface InvoiceCounterpartNameProps {
   counterpartId: string | undefined;
 }
 
-export const InvoiceCounterpartName = ({
-  counterpartId,
-}: InvoiceCounterpartNameProps) => {
-  const {
-    data: counterpart,
-    isLoading,
-    error,
-  } = useCounterpartById(counterpartId);
+export const InvoiceCounterpartName = memo(
+  ({ counterpartId }: InvoiceCounterpartNameProps) => {
+    const {
+      data: counterpart,
+      isLoading,
+      error,
+    } = useCounterpartById(counterpartId);
 
-  if (isLoading) {
-    return <Skeleton variant="text" height="50%" width="70%" />;
+    if (isLoading) {
+      return <Skeleton variant="text" height="50%" width="70%" />;
+    }
+
+    if (error) {
+      return <>—</>;
+    }
+
+    return <>{getCounterpartName(counterpart)}</>;
   }
-
-  if (error) {
-    return <>—</>;
-  }
-
-  return <>{getCounterpartName(counterpart)}</>;
-};
+);
