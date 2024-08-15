9efbf924798da0a3e00fb9786a4db2f9c3a0ece6
Alex Batalov <radist2s@gmail.com>
2024-08-15 04:10:08 +0400

    fix(DEV-11417): move `<MoniteAPIProvider/>` to `MoniteAPIProvider` from `<MoniteScopedProviders/>` to prevent duplicated `fetchToken` calls when navigating between Super Components

--- packages/sdk-react/src/core/context/MoniteScopedProviders.tsx
+++ packages/sdk-react/src/core/context/MoniteScopedProviders.tsx
@@ -1,10 +1,6 @@
 import { createContext, ReactNode, useContext } from 'react';
 
 import { EmotionCacheProvider } from '@/core/context/EmotionCacheProvider';
-import {
-  MoniteAPIProvider,
-  MoniteQraftContext,
-} from '@/core/context/MoniteAPIProvider';
 import { useMoniteContext } from '@/core/context/MoniteContext';
 import { MoniteI18nProvider } from '@/core/context/MoniteI18nProvider';
 import { MoniteQueryClientProvider } from '@/core/context/MoniteQueryClientProvider';
@@ -34,17 +30,7 @@ export const MoniteScopedProviders = ({
         <MoniteI18nProvider>
           <MuiThemeProvider theme={theme}>
             <SentryProvider>
-              <MoniteQueryClientProvider>
-                <MoniteAPIProvider
-                  apiUrl={apiUrl}
-                  fetchToken={fetchToken}
-                  requestFn={requestFn}
-                  queryClient={queryClient}
-                  APIContext={MoniteQraftContext}
-                >
-                  {children}
-                </MoniteAPIProvider>
-              </MoniteQueryClientProvider>
+              <MoniteQueryClientProvider>{children}</MoniteQueryClientProvider>
             </SentryProvider>
           </MuiThemeProvider>
         </MoniteI18nProvider>
