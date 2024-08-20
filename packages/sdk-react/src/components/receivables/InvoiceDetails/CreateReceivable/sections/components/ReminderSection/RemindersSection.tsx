fce5ab9d3d9c182ef366e4538a1dcecc1ff749f3
Vitaly Kuprin <vitas_s@inbox.lv>
2024-08-20 19:32:58 +0300

    chore(DEV-12084): return alerts

--- packages/sdk-react/src/components/receivables/InvoiceDetails/CreateReceivable/sections/components/ReminderSection/RemindersSection.tsx
+++ packages/sdk-react/src/components/receivables/InvoiceDetails/CreateReceivable/sections/components/ReminderSection/RemindersSection.tsx
@@ -12,6 +12,7 @@ import { t } from '@lingui/macro';
 import { useLingui } from '@lingui/react';
 import AddIcon from '@mui/icons-material/Add';
 import {
+  Alert,
   Button,
   Card,
   CardContent,
@@ -168,6 +169,20 @@ const ReminderSectionContent = ({
 
   return (
     <>
+      {!hasValidReminderEmailLoading && Boolean(counterpartId) && (
+        <>
+          {!counterpart?.reminders_enabled && (
+            <Alert severity="warning" sx={{ mb: 2 }}>{t(
+              i18n
+            )`Reminders are disabled for this Counterpart.`}</Alert>
+          )}
+          {!hasValidReminderEmail && (
+            <Alert severity="warning" sx={{ mb: 2 }}>{t(
+              i18n
+            )`No default email for selected Counterpart. Reminders will not be sent.`}</Alert>
+          )}
+        </>
+      )}
       <Grid container spacing={3}>
         <Grid item xs={12} sm={6}>
           <SelectReminderLayout
