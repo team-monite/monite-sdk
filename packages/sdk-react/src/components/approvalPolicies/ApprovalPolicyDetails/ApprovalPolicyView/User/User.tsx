71ff6da19070ce2b6406db259b1c2bb5502ba813
Andrei Tarasov <tarasov.a.dev@gmail.com>
2024-08-21 20:33:42 +0200

    feat(DEV-12330): view trigger details

---
+++ packages/sdk-react/src/components/approvalPolicies/ApprovalPolicyDetails/ApprovalPolicyView/User/User.tsx
@@ -0,0 +1,29 @@
+import { UserAvatar } from '@/components/UserAvatar/UserAvatar';
+import { useEntityUserById } from '@/core/queries';
+import { Stack, Skeleton } from '@mui/material';
+
+interface UserProps {
+  userId: string;
+}
+
+export const User = ({ userId }: UserProps) => {
+  const { data: entityUser, isLoading } = useEntityUserById(userId);
+
+  if (!entityUser) {
+    return null;
+  }
+
+  if (isLoading) {
+    return <Skeleton height="50%" width={100} animation="wave" />;
+  }
+
+  return (
+    <Stack direction="row" alignItems="center" gap={1}>
+      <UserAvatar
+        fileId={entityUser.userpic_file_id}
+        sx={{ width: 24, height: 24 }}
+      />
+      {`${entityUser.first_name ?? ''} ${entityUser.last_name ?? ''}`.trim()}
+    </Stack>
+  );
+};
