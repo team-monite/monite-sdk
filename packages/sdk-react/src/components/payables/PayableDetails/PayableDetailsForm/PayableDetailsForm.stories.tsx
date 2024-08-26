01e91e0ddb9e18e5d75cf1592334481e53af2a43
Vitaly Kuprin <51091179+vkuprin@users.noreply.github.com>
2024-08-26 15:10:34 +0200

    feat(DEV-12181): configure custom fields that need to be mandatory after OCR for user to check (#253)
    * feat(DEV-12181): add ocr required fields
    
    * chore(DEV-12181): add default ocr required as was
    
    * style(DEV-12181): match mui error
    
    * style(DEV-12181): remove overriding red styles
    
    * chore(DEV-12181): trigger validation on form load
    
    * style(DEV-12181): highlight from patable info as well
    
    * style(DEV-12181): mark more values
    
    * chore(DEV-12181): modify to pass only by value
    
    * docs(changeset): feat(DEV-12181): configure custom fields that need to be mandatory after OCR for user to check
    
    * fix(DEV-12181): unit tests
    
    * chore(DEV-12181): code review - remove false values
    
    * chore(DEV-12181): avoid dry
    
    * chore(DEV-12181): follow more DRY
    
    * chore(DEV-12181): more precise typings
    
    * chore(DEV-12181): fix theme customization
    
    * fix(DEV-12181): linting
    
    * fix(DEV-12181): type checking
    
    * chore(DEV-12181): fix some code review moments
    
    * chore(DEV-12181): modify on correct hook
    
    * chore(DEV-12181): simplify is field required
    
    * chore(DEV-12181): add ts doc
    
    * chore(DEV-12181): early return for false
    
    * chore(DEV-12181): type check change fields
    
    * chore(DEV-12181): add proper generics
    
    * chore(DEV-12181): skeleton storybook
    
    * chore(DEV-12181): refactor story
    
    * chore(DEV-12181): change trigger case
    
    * chore(DEV-12181): cleanups
    
    * chore(DEV-12181): red labels mark
    
    * chore(DEV-12181): fix linting
    
    * chore(DEV-12181): fix import and to never type
    
    * chore(DEV-12181): modify for code review
    
    * chore(DEV-12181): avoid extra component
    
    * chore(DEV-12181): fix linting
    
    * chore(DEV-12181): mui ts doc
    
    * chore(DEV-12181): fields as required only if payable.ocr_status is empty
    
    * chore(DEV-12181): fix style duplication
    
    * chore: fix unused imports
    
    * fix: theme extension of the MonitePayableDetailsInfo
    
    * fix: explicitly specify fields that can be required
    
    * fix: OcrFields typings
    
    ---------
    
    Co-authored-by: Ivan Nikitin <cherniy.bumer@gmail.com>
    Co-authored-by: ivannikitin <ivan@nikitin.io>

---
+++ packages/sdk-react/src/components/payables/PayableDetails/PayableDetailsForm/PayableDetailsForm.stories.tsx
@@ -0,0 +1,74 @@
+import { Dialog } from '@/components/Dialog';
+import { ExtendThemeProvider } from '@/utils/ExtendThemeProvider';
+import { action } from '@storybook/addon-actions';
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { PayableDetailsForm } from './PayableDetailsForm';
+
+const meta: Meta<typeof PayableDetailsForm> = {
+  title: 'Payables/Payables — Details Form',
+  component: PayableDetailsForm,
+};
+
+const payableId = '9a3b97a5-a1ba-4d8c-bade-ad3c47ae61e0';
+
+type Story = StoryObj<typeof PayableDetailsForm>;
+
+const actions = {
+  onSavePayable: action('onSavePayable'),
+  onCreatePayable: action('onCreatePayable'),
+};
+
+export const DialogDetailsForm: Story = {
+  args: {
+    payableDetailsFormId: 'payable-form-2',
+    lineItems: [
+      {
+        payable_id: payableId,
+        id: 'item-2',
+        name: 'Service B',
+        quantity: 2,
+        tax: 20,
+      },
+    ],
+    ...actions,
+  },
+  render: (args) => {
+    return (
+      <Dialog
+        open={true}
+        fullScreen
+        onClose={() => {
+          action('onClose')();
+        }}
+        onSubmit={() => {}}
+        onClosed={action('onClosed')}
+      >
+        <ExtendThemeProvider
+          theme={{
+            components: {
+              MonitePayableDetailsInfo: {
+                defaultProps: {
+                  optionalFields: {
+                    invoiceDate: true, // Show the invoice date field
+                    tags: true, // Show the tags field
+                  },
+                  ocrRequiredFields: {
+                    invoiceNumber: true, // The invoice number is required based on OCR data
+                    counterpart: true, // The counterpart is required based on OCR data
+                    dueDate: true, // The due date is required based on OCR data
+                    currency: true, // The currency is required based on OCR data
+                  },
+                },
+              },
+            },
+          }}
+        >
+          <PayableDetailsForm {...args} />
+        </ExtendThemeProvider>
+      </Dialog>
+    );
+  },
+};
+
+export default meta;
