f8c7b98e6b62029d7471f4a1fabe2634b9719a27
Vitaly Kuprin <vitas_s@inbox.lv>
2024-08-26 14:50:55 +0300

    chore(DEV-12180): mark overdue date

--- packages/sdk-react/src/components/payables/utils/isInvoiceOverdue.ts
+++ packages/sdk-react/src/components/payables/utils/isInvoiceOverdue.ts
@@ -1,12 +1,18 @@
 import { components } from '@/api';
 
 export const isInvoiceOverdue = (
-  invoice: components['schemas']['InvoiceResponsePayload']
+  invoice: components['schemas']['PayableResponseSchema']
 ) => {
   const today = new Date();
+  const statuses: components['schemas']['PayableStateEnum'][] = [
+    'draft',
+    'new',
+    'waiting_to_be_paid',
+    'approve_in_progress',
+  ];
+
   return (
-    // eslint-disable-next-line lingui/no-unlocalized-strings
-    ['new', 'pending', 'waiting to be paid'].includes(invoice.status) &&
+    statuses.includes(invoice.status) &&
     invoice.due_date &&
     new Date(invoice.due_date).getTime() < today.getTime()
   );
