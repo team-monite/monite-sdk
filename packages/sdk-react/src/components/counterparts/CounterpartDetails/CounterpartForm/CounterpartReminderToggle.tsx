04524bf68918b4a852830e4bd7a7a8e195e71eb1
Vitaly Kuprin <51091179+vkuprin@users.noreply.github.com>
2024-08-26 12:43:45 +0200

    feat(DEV-12368): Implement toggle to turn reminders on/off on counterpart level (#274)
    * chore(DEV-12368): design part for toggles
    
    * chore(DEV-12368): missing trans
    
    * chore(DEV-12368): cleanups
    
    * chore(DEV-12368): modify current tests
    
    * docs(changeset): feat(DEV-12368): Implement toggle to turn reminders on/off on counterpart leve
    
    * Update afraid-ties-suffer.md
    
    * chore(DEV-12368): provide `control` directly to `<CounterpartReminderToggle/>` component
    
    * chore(DEV-12368): simplify `<CounterpartReminderToggle/>` layout
    
    * chore(DEV-12368): add reminders usage in form for `CounterpartIndividualForm` and `CounterpartOrganizationForm` components
    
    ---------
    
    Co-authored-by: Alex Batalov <radist2s@gmail.com>

---
+++ packages/sdk-react/src/components/counterparts/CounterpartDetails/CounterpartForm/CounterpartReminderToggle.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import {
+  Controller,
+  type FieldValues,
+  type UseControllerProps,
+} from 'react-hook-form';
+
+import { t } from '@lingui/macro';
+import { useLingui } from '@lingui/react';
+import { Box, Switch, Typography } from '@mui/material';
+
+export const CounterpartReminderToggle = <T extends FieldValues>(
+  props: UseControllerProps<T>
+) => {
+  const { i18n } = useLingui();
+
+  return (
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="space-between"
+      padding={2}
+      sx={{
+        borderRadius: 2,
+        border: '1px solid',
+        borderColor: 'divider',
+      }}
+    >
+      <Box>
+        <Typography variant="subtitle1">
+          {t(i18n)`Enable email reminders`}
+        </Typography>
+        <Typography variant="body2" color="textSecondary">
+          {t(
+            i18n
+          )`You can change this setting if you wish to forbid sending any payment reminders to this counterpart.`}
+        </Typography>
+      </Box>
+      <Controller
+        {...props}
+        render={({ field }) => (
+          <Switch
+            checked={field.value}
+            onChange={(event) => field.onChange(event.target.checked)}
+            color="primary"
+            aria-label={t(i18n)`Enable email reminders`}
+          />
+        )}
+      />
+    </Box>
+  );
+};
