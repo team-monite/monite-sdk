1bd3ffcf712a8e438b30f9660bc660188a4408ce
Vitaly Kuprin <vitas_s@inbox.lv>
2024-08-27 16:19:53 +0300

    chore(DEV-12180): modify is overdue upload test cases

--- packages/sdk-react/src/components/payables/utils/isInvoiceOverdue.ts
+++ packages/sdk-react/src/components/payables/utils/isInvoiceOverdue.ts
@@ -3,8 +3,10 @@ import { components } from '@/api';
 export const isInvoiceOverdue = (
   invoice: components['schemas']['PayableResponseSchema']
 ) => {
+  if (!invoice.due_date) return false;
+
   const today = new Date();
-  today.setHours(0, 0, 0, 0); // we are comparing only the date without considering the time
+  today.setUTCHours(0, 0, 0, 0); // Ensure today is midnight UTC
 
   const statuses: components['schemas']['PayableStateEnum'][] = [
     'draft',
@@ -13,10 +15,12 @@ export const isInvoiceOverdue = (
     'approve_in_progress',
   ];
 
+  const dueDate = new Date(invoice.due_date);
+  dueDate.setUTCHours(0, 0, 0, 0); // Normalize dueDate to midnight UTC
+
   return (
     statuses.includes(invoice.status) &&
     invoice.due_date &&
-    new Date(invoice.due_date).getTime() < today.getTime() &&
-    new Date(invoice.due_date).getTime() < today.getTime() + 24 * 60 * 60 * 1000
+    dueDate.getTime() < today.getTime() + 24 * 60 * 60 * 1000
   );
 };
