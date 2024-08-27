5638a61f59f6cd69bdd110649f4008fbf82daa17
ivannikitin <ivan@nikitin.io>
2024-08-27 14:52:57 +0200

    chore: refactor hook

--- packages/sdk-react/src/core/hooks/index.ts
+++ packages/sdk-react/src/core/hooks/index.ts
@@ -2,3 +2,5 @@ export * from './useCurrencies';
 export * from './useOptionalFields';
 export * from './useMenuButton';
 export * from './useFileInput';
+export * from './useAutosizeGridColumns';
+export * from './useFormPersist';
