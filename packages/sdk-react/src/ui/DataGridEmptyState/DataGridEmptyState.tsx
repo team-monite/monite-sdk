e122b16383e45c40dc4d00371503b1afa75dc12f
Vitaly Kuprin <vitas_s@inbox.lv>
2024-08-29 13:16:37 +0300

    chore(DEV-12417): predefine hardcoded values

--- packages/sdk-react/src/ui/DataGridEmptyState/DataGridEmptyState.tsx
+++ packages/sdk-react/src/ui/DataGridEmptyState/DataGridEmptyState.tsx
@@ -4,24 +4,40 @@ import { useMenuButton } from '@/core/hooks';
 import { CenteredContentBox } from '@/ui/box';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+import NoDataIcon from '@mui/icons-material/Block';
+import ErrorIcon from '@mui/icons-material/ErrorOutline';
+import CountryUnsupportedIcon from '@mui/icons-material/HourglassEmpty';
+import AccessRestrictedIcon from '@mui/icons-material/Lock';
 import RefreshIcon from '@mui/icons-material/Refresh';
 import { Box, Button, Menu, MenuItem, Stack, Typography } from '@mui/material';
 
-export interface DataGridEmptyStateProps {
+interface BaseProps {
+  onAction?: (option?: string) => void;
+  actionButtonLabel?: string;
+  actionOptions?: string[];
+  type:
+    | 'no-data'
+    | 'error'
+    | 'access-restricted'
+    | 'unsupported-country'
+    | 'custom';
+  className?: string;
+}
+
+export interface DataGridEmptyStateProps extends BaseProps {
   icon?: ReactNode;
   title: string;
   descriptionLine1: string;
   descriptionLine2?: string;
+}
+
+interface ActionButtonProps extends BaseProps {
   onAction?: (option?: string) => void;
-  actionButtonLabel?: string;
-  actionOptions?: string[];
-  type: 'no-data' | 'error' | 'access-restricted' | 'unsupported-country';
-  className?: string;
 }
 
 export const DataGridEmptyState = ({
-  icon,
   title,
+  icon,
   descriptionLine1,
   descriptionLine2,
   onAction,
@@ -29,41 +45,68 @@ export const DataGridEmptyState = ({
   actionOptions = [],
   type,
   className = 'Monite-DataGridEmptyState',
-}: DataGridEmptyStateProps) => (
-  <CenteredContentBox className={`${className}-Content`}>
-    <Stack alignItems="center" spacing={2}>
-      {icon && <Box className={`${className}-Icon`}>{icon}</Box>}
-      <Stack alignItems="center" spacing={1}>
-        <Typography variant="h5" className={`${className}-Title`}>
-          {title}
-        </Typography>
-        <Typography className={`${className}-Description`}>
-          {descriptionLine1}
-        </Typography>
-        {descriptionLine2 && (
-          <Typography className={`${className}-Description`}>
-            {descriptionLine2}
-          </Typography>
-        )}
-      </Stack>
-      <ActionButton
-        onAction={onAction}
-        actionButtonLabel={actionButtonLabel}
-        actionOptions={actionOptions}
-        type={type}
-        className={className}
-      />
-    </Stack>
-  </CenteredContentBox>
-);
+}: DataGridEmptyStateProps) => {
+  let defaultIcon;
+  switch (type) {
+    case 'error':
+      defaultIcon = (
+        <ErrorIcon sx={{ fontSize: '4rem', color: 'error.main', mb: 2 }} />
+      );
+      break;
+    case 'no-data':
+      defaultIcon = (
+        <NoDataIcon sx={{ fontSize: '4rem', color: 'primary.main', mb: 2 }} />
+      );
+      break;
+    case 'access-restricted':
+      defaultIcon = (
+        <AccessRestrictedIcon
+          sx={{ fontSize: '4rem', color: 'warning.main', mb: 2 }}
+        />
+      );
+      break;
+    case 'unsupported-country':
+      defaultIcon = (
+        <CountryUnsupportedIcon
+          sx={{ fontSize: '4rem', color: 'grey.500', mb: 2 }}
+        />
+      );
+      break;
+    default:
+      defaultIcon = null;
+  }
 
-interface ActionButtonProps {
-  onAction?: (option?: string) => void;
-  actionButtonLabel?: string;
-  actionOptions?: string[];
-  type: 'no-data' | 'error' | 'access-restricted' | 'unsupported-country';
-  className?: string;
-}
+  return (
+    <CenteredContentBox className={`${className}-Content`}>
+      <Stack alignItems="center" spacing={2}>
+        {icon ||
+          (defaultIcon && (
+            <Box className={`${className}-Icon`}>{icon || defaultIcon}</Box>
+          ))}
+        <Stack alignItems="center" spacing={1}>
+          <Typography variant="h5" className={`${className}-Title`}>
+            {title}
+          </Typography>
+          <Typography className={`${className}-Description`}>
+            {descriptionLine1}
+          </Typography>
+          {descriptionLine2 && (
+            <Typography className={`${className}-Description`}>
+              {descriptionLine2}
+            </Typography>
+          )}
+        </Stack>
+        <ActionButton
+          onAction={onAction}
+          actionButtonLabel={actionButtonLabel}
+          actionOptions={actionOptions}
+          type={type}
+          className={className}
+        />
+      </Stack>
+    </CenteredContentBox>
+  );
+};
 
 const ActionButton = ({
   onAction,
