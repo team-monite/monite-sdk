29ef4a87c1350b3e4cc50c9e72ac27e2af9d1297
Andrei Tarasov <tarasov.a.dev@gmail.com>
2024-11-17 23:15:00 +0100

    refactor(DEV-12144): remove sdk-api from the repository

--- packages/sdk-api/src/api/models/ReceivableSettingsResponse.ts
+++
@@ -1,8 +0,0 @@
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-export type ReceivableSettingsResponse = {
-  create_without_personal_info: boolean;
-  deduction_title?: string;
-};
--- packages/sdk-api/src/api/models/Reminder.ts
+++
@@ -1,9 +0,0 @@
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-export type Reminder = {
-  body: string;
-  days_before: number;
-  subject: string;
-};
