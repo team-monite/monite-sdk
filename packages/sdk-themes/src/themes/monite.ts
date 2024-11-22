ed85f9174a72583275dc0899001f8b108f1abbdb
Marc Perez <marc.perez@monite.com>
2024-11-22 15:37:06 +0100

    Show pointer on table rows

--- packages/sdk-themes/src/themes/monite.ts
+++ packages/sdk-themes/src/themes/monite.ts
@@ -829,6 +829,11 @@ export const defaultMoniteComponents: Components<Omit<Theme, 'components'>> = {
           lineHeight: '17.57px',
         },
       },
+      row: {
+        '&:hover': {
+          cursor: 'pointer',
+        },
+      },
       cell: {
         '&[role="gridcell"]': {
           borderLeftStyle: 'solid',
