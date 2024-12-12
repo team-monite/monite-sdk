c03bb74a38fd7f900a352bd70aabe1a525d803b4
Precious OSSAI <theossaiprecious@gmail.com>
2024-12-12 11:07:54 +0000

    chore

--- packages/sdk-react/src/core/utils/createThemeWithDefaults.ts
+++ packages/sdk-react/src/core/utils/createThemeWithDefaults.ts
@@ -37,7 +37,6 @@ export const createThemeWithDefaults = (
               },
               {
                 label: FINANCING_LABEL,
-                query: { type: 'financing' },
               },
             ],
           },
