e4e5ced98506a7a17bd0a3fc39c64f618dac6f80
Andrei Tarasov <tarasov.a.dev@gmail.com>
2024-12-27 15:01:36 +0100

    feat(DEV-13634): move icon wrapper from theme to component settings

--- packages/sdk-react/src/core/componentSettings/index.ts
+++ packages/sdk-react/src/core/componentSettings/index.ts
@@ -1,10 +1,48 @@
-import { ComponentSettings } from '@/core/context/MoniteProvider';
+import type { MoniteIconWrapperProps } from '@/ui/iconWrapper';
+
+export interface ComponentSettings {
+  general: {
+    iconWrapper: Partial<MoniteIconWrapperProps>;
+  };
+  approvalPolicies: {
+    pageSizeOptions: number[];
+  };
+  approvalRequests: {
+    pageSizeOptions: number[];
+  };
+  counterparts: {
+    pageSizeOptions: number[];
+  };
+  payables: {
+    pageSizeOptions: number[];
+  };
+  products: {
+    pageSizeOptions: number[];
+  };
+  receivables: {
+    pageSizeOptions: number[];
+  };
+  tags: {
+    pageSizeOptions: number[];
+  };
+  userRoles: {
+    pageSizeOptions: number[];
+  };
+}
 
 const defaultPageSizeOptions = [15, 30, 100];
 
 export const getDefaultComponentSettings = (
   componentSettings?: Partial<ComponentSettings>
 ) => ({
+  general: {
+    iconWrapper: {
+      defaultProps: {
+        showCloseIcon: true,
+      },
+      ...componentSettings?.general?.iconWrapper,
+    },
+  },
   approvalPolicies: {
     pageSizeOptions:
       componentSettings?.approvalPolicies?.pageSizeOptions ||
