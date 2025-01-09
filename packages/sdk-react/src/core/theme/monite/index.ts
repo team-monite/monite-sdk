051d682ff41ad79a2f429c892f416ce89f90c6b9
Andrei Tarasov <tarasov.a.dev@gmail.com>
2025-01-09 20:15:42 +0100

    feat(DEV-13634): update primary colors calculation

--- packages/sdk-react/src/core/theme/monite/index.ts
+++ packages/sdk-react/src/core/theme/monite/index.ts
@@ -2,8 +2,8 @@ import { ThemeConfig } from '@/core/theme/types';
 
 export const getTheme = (theme: ThemeConfig) => {
   return {
-    borderRadius: theme.borderRadius || 3,
-    spacing: theme.spacing || 8,
+    borderRadius: theme.borderRadius ?? 3,
+    spacing: theme.spacing ?? 8,
 
     colors: {
       primary: theme.colors?.primary || '#3737FF',
@@ -25,42 +25,42 @@ export const getTheme = (theme: ThemeConfig) => {
         theme?.typography?.fontFamily ||
         // eslint-disable-next-line lingui/no-unlocalized-strings
         '"Faktum", -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif',
-      fontSize: theme?.typography?.fontSize || 16,
+      fontSize: theme?.typography?.fontSize ?? 16,
 
       h1: {
-        fontSize: theme.typography?.h1?.fontSize || 48,
-        fontWeight: theme.typography?.h1?.fontWeight || 600,
-        lineHeight: theme.typography?.h1?.lineHeight || '68px',
+        fontSize: theme.typography?.h1?.fontSize ?? 48,
+        fontWeight: theme.typography?.h1?.fontWeight ?? 600,
+        lineHeight: theme.typography?.h1?.lineHeight ?? '68px',
       },
       h2: {
-        fontSize: theme.typography?.h2?.fontSize || 34,
-        fontWeight: theme.typography?.h2?.fontWeight || 640,
-        lineHeight: theme.typography?.h2?.lineHeight || '40px',
+        fontSize: theme.typography?.h2?.fontSize ?? 34,
+        fontWeight: theme.typography?.h2?.fontWeight ?? 640,
+        lineHeight: theme.typography?.h2?.lineHeight ?? '40px',
       },
       h3: {
-        fontSize: theme.typography?.h2?.fontSize || 24,
-        fontWeight: theme.typography?.h2?.fontWeight || 600,
-        lineHeight: theme.typography?.h2?.lineHeight || '32px',
+        fontSize: theme.typography?.h2?.fontSize ?? 24,
+        fontWeight: theme.typography?.h2?.fontWeight ?? 600,
+        lineHeight: theme.typography?.h2?.lineHeight ?? '32px',
       },
       subtitle1: {
-        fontSize: theme.typography?.h2?.fontSize || 20,
-        fontWeight: theme.typography?.h2?.fontWeight || 600,
-        lineHeight: theme.typography?.h2?.lineHeight || '24px',
+        fontSize: theme.typography?.h2?.fontSize ?? 20,
+        fontWeight: theme.typography?.h2?.fontWeight ?? 600,
+        lineHeight: theme.typography?.h2?.lineHeight ?? '24px',
       },
       subtitle2: {
-        fontSize: theme.typography?.h2?.fontSize || 18,
-        fontWeight: theme.typography?.h2?.fontWeight || 600,
-        lineHeight: theme.typography?.h2?.lineHeight || '24px',
+        fontSize: theme.typography?.h2?.fontSize ?? 18,
+        fontWeight: theme.typography?.h2?.fontWeight ?? 600,
+        lineHeight: theme.typography?.h2?.lineHeight ?? '24px',
       },
       body1: {
-        fontSize: theme.typography?.body1?.fontSize || 16,
-        fontWeight: theme.typography?.body1?.fontWeight || 500,
-        lineHeight: theme.typography?.body1?.lineHeight || '24px',
+        fontSize: theme.typography?.body1?.fontSize ?? 16,
+        fontWeight: theme.typography?.body1?.fontWeight ?? 500,
+        lineHeight: theme.typography?.body1?.lineHeight ?? '24px',
       },
       body2: {
-        fontSize: theme.typography?.body2?.fontSize || 14,
-        fontWeight: theme.typography?.body2?.fontWeight || 400,
-        lineHeight: theme.typography?.body2?.lineHeight || '20px',
+        fontSize: theme.typography?.body2?.fontSize ?? 14,
+        fontWeight: theme.typography?.body2?.fontWeight ?? 400,
+        lineHeight: theme.typography?.body2?.lineHeight ?? '20px',
       },
     },
 
