6bb873df6b5aa16669402c976c8c37db3676aeab
Andrei Tarasov <tarasov.a.dev@gmail.com>
2025-01-09 20:15:42 +0100

    feat(DEV-13634): update primary colors calculation

--- packages/sdk-react/src/core/theme/mui-monite/colors/primary.ts
+++ packages/sdk-react/src/core/theme/mui-monite/colors/primary.ts
@@ -2,11 +2,11 @@ import chroma from 'chroma-js';
 
 export const getPrimaryColors = (mainColor: string) => {
   return {
-    dark: chroma(mainColor).darken(0.8).hex(),
+    dark: chroma(mainColor).darken(0.3).saturate(0.2).hex(),
     main: mainColor,
-    light: chroma(mainColor).brighten(2.5).desaturate(1).hex(),
-    '60': chroma(mainColor).alpha(0.6).hex(),
-    '80': chroma(mainColor).brighten(1.8).desaturate(0.3).hex(),
-    '90': chroma(mainColor).brighten(2.2).desaturate(0.2).hex(),
+    light: chroma.mix(mainColor, '#ffffff', 0.94).hex(),
+    '60': chroma.mix(mainColor, '#ffffff', 0.4).desaturate(0.2).hex(), // #9999FF
+    '80': chroma.mix(mainColor, '#ffffff', 0.7).desaturate(0.4).hex(), // #CBCBFE
+    '90': chroma.mix(mainColor, '#ffffff', 0.9).desaturate(0.6).hex(), // #EBEBFF
   };
 };
