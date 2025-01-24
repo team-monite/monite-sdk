1bba7d9f47e0a48c9a6c7b832663764a682763bc
oneanotheruser <oneanotheruser@gmail.com>
2025-01-24 21:07:14 +0100

    work on document design;

--- packages/sdk-react/src/components/documentDesign/DocumentDesign.stories.tsx
+++ packages/sdk-react/src/components/documentDesign/DocumentDesign.stories.tsx
@@ -1,30 +1,30 @@
 import { css } from '@emotion/react';
-import { StoryObj } from '@storybook/react';
+import { StoryObj } from "@storybook/react";
 
-import { DocumentDesign as DocumentDesignComponent } from './DocumentDesign';
+import { DocumentDesign as DocumentDesignComponent } from "./DocumentDesign";
 
 const Story = {
-  title: 'Document design/Document design',
-  component: DocumentDesignComponent,
+    title: 'Document design',
+    component: DocumentDesignComponent,
 };
 
 type Story = StoryObj<typeof DocumentDesignComponent>;
 
 export const DocumentDesign: Story = {
-  args: {},
-  render: () => (
-    <div
-      css={css`
-        box-sizing: border-box;
-        display: flex;
-        flex-direction: column;
-        height: 100vh;
-        padding: 20px;
-      `}
-    >
-      <DocumentDesignComponent />
-    </div>
-  ),
+    args: {},
+    render: () => (
+        <div
+            css={css`
+                box-sizing: border-box;
+                display: flex;
+                flex-direction: column;
+                height: 100vh;
+                padding: 20px;
+            `}
+        >
+            <DocumentDesignComponent />
+        </div>
+    ),
 };
 
 export default Story;
--- packages/sdk-react/src/components/documentDesign/DocumentDesign.tsx
+++ packages/sdk-react/src/components/documentDesign/DocumentDesign.tsx
@@ -1,13 +1,37 @@
+import { Box, Typography } from '@mui/material';
+import { PageHeader } from '@/components';
 import { MoniteScopedProviders } from '@/core/context/MoniteScopedProviders';
-
-import { DocumentDesignSelection } from './components/DocumentDesignSelection/DocumentDesignSelection';
+import { t } from '@lingui/macro';
+import { useLingui } from '@lingui/react';
 
 const DocumentDesignBase = () => {
-  return <DocumentDesignSelection />;
+    const { i18n } = useLingui();
+
+    return (
+        <>
+            <PageHeader title={t(i18n)`Documet design`}/>
+            <Box
+                padding={2}
+                sx={{
+                  borderRadius: 2,
+                  border: '1px solid',
+                  borderColor: 'divider',
+                  background: 'linear-gradient(rgb(244, 240, 254), rgb(255, 255, 255))'
+                }}
+            >
+                <Typography variant="subtitle2">
+                  {t(i18n)`Set your document style`}
+                </Typography>
+                <Typography variant="caption">
+                  {t(i18n)`Various templates for your documents`}
+                </Typography>
+            </Box>
+        </>
+    );
 };
 
 export const DocumentDesign = () => (
-  <MoniteScopedProviders>
-    <DocumentDesignBase />
-  </MoniteScopedProviders>
+    <MoniteScopedProviders>
+        <DocumentDesignBase />
+    </MoniteScopedProviders>
 );
