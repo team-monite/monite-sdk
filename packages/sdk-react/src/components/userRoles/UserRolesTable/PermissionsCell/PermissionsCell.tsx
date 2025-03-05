35bc24c808d446cc9853c2038359b67e5968e256
Dinis Areias <dinis.areias@monite.com>
2025-03-05 17:24:32 +0000

    Change User Roles table to only show permissions with allowed actions

--- packages/sdk-react/src/components/userRoles/UserRolesTable/PermissionsCell/PermissionsCell.tsx
+++ packages/sdk-react/src/components/userRoles/UserRolesTable/PermissionsCell/PermissionsCell.tsx
@@ -2,7 +2,7 @@ import { components } from '@/api';
 import { getPermissionToLabelMap } from '@/components/userRoles/consts';
 import { t } from '@lingui/macro';
 import { useLingui } from '@lingui/react';
-import { Grid, Link } from '@mui/material';
+import { Box, Grid } from '@mui/material';
 
 import { Permission } from './Permission';
 
@@ -17,28 +17,36 @@ interface PermissionsCellProps {
 export const PermissionsCell = ({ permissions }: PermissionsCellProps) => {
   const { i18n } = useLingui();
 
-  if (!permissions.objects) {
-    return null;
+  const filteredPermissionsObjects = permissions.objects
+    ?.filter((object) => !!object.object_type)
+    .filter(
+      (object) =>
+        object.actions &&
+        !object.actions.every((action) => action.permission == 'not_allowed')
+    );
+
+  if (!filteredPermissionsObjects?.length) {
+    return <Box>{t(i18n)`None`}</Box>;
   }
 
   return (
     <Grid container>
       {filteredPermissionsObjects.map((object) => (
-          <Grid item container key={object.object_type}>
-            <Grid item xs={6}>
-              {
-                getPermissionToLabelMap(i18n)[
-                  object.object_type as keyof ReturnType<
-                    typeof getPermissionToLabelMap
-                  >
-                ]
-              }
-            </Grid>
-            <Grid item xs={6}>
-              {object.actions && <Permission actions={object.actions} />}
-            </Grid>
+        <Grid item container key={object.object_type}>
+          <Grid item xs={6}>
+            {
+              getPermissionToLabelMap(i18n)[
+                object.object_type as keyof ReturnType<
+                  typeof getPermissionToLabelMap
+                >
+              ]
+            }
           </Grid>
-        ))}
+          <Grid item xs={6}>
+            {object.actions && <Permission actions={object.actions} />}
+          </Grid>
+        </Grid>
+      ))}
     </Grid>
   );
 };
