ee264edb14388a11ba4cb05f3c866f62fbed564d
Dinis Areias <dinis.areias@monite.com>
2025-03-05 17:23:17 +0000

    Fix User Roles table date filter null value

--- packages/sdk-react/src/components/userRoles/UserRolesTable/Filters/Filters.tsx
+++ packages/sdk-react/src/components/userRoles/UserRolesTable/Filters/Filters.tsx
@@ -44,7 +44,7 @@ export const Filters = ({ onChangeFilter, sx }: FiltersProps) => {
             return;
           }
 
-          if (value instanceof Date) {
+          if (value instanceof Date || value === null) {
             onChangeFilter(FILTER_TYPE_CREATED_AT, value);
           }
         }}
