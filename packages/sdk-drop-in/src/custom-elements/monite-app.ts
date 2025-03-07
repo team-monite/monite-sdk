08025d87028204dfebab4ce4db5e7350d8454ad9
Marc Perez <marc.perez@monite.com>
2025-03-07 09:55:55 +0100

    New dropin syntax

--- packages/sdk-drop-in/src/custom-elements/monite-app.ts
+++ packages/sdk-drop-in/src/custom-elements/monite-app.ts
@@ -1,3 +1,8 @@
+import { ComponentProps } from 'react';
+
+import { WidgetType } from '@/apps/MoniteApp';
+import type { APISchema, MoniteProvider } from '@monite/sdk-react';
+
 import {
   MoniteEventTypes,
   addMoniteEventListener,
@@ -9,9 +14,105 @@ import { MoniteAppElement } from './MoniteAppElement';
 
 const MONITE_APP_ELEMENT_NAME = 'monite-app';
 
-export const registerMoniteAppElement = () => {
-  customElements.define(MONITE_APP_ELEMENT_NAME, MoniteAppElement);
-};
+customElements.define(MONITE_APP_ELEMENT_NAME, MoniteAppElement);
+
+interface MoniteAppElementConfig {
+  /**
+   * Entity ID
+   * @type {string}
+   */
+  entityId: string;
+
+  /**
+   * API URL
+   * @type {string}
+   */
+  apiUrl: string;
+
+  /**
+   * Function to fetch the access token
+   * @returns {Promise<APISchema.components['schemas']['AccessTokenResponse']>}
+   */
+  fetchToken: () =>
+    | Promise<APISchema.components['schemas']['AccessTokenResponse']>
+    | string;
+
+  /**
+   * Locale overrides
+   * @type {string}
+   */
+  locale?: ComponentProps<typeof MoniteProvider>['locale'];
+
+  /**
+   * Theme overrides
+   * @type {ThemeConfig}
+   */
+  theme?: ComponentProps<typeof MoniteProvider>['theme'];
+
+  /**
+   * Component settings overrides
+   * @type {ComponentSettings}
+   */
+  componentSettings?: ComponentProps<
+    typeof MoniteProvider
+  >['componentSettings'];
+}
+
+class MoniteDropin {
+  private moniteAppElement: HTMLElement;
+  private config: MoniteAppElementConfig;
+
+  constructor(config: MoniteAppElementConfig) {
+    this.config = config;
+    this.moniteAppElement = document.createElement(
+      MONITE_APP_ELEMENT_NAME
+    ) as HTMLElement;
+  }
+
+  public create(component: WidgetType) {
+    this.moniteAppElement.setAttribute('entity-id', this.config.entityId);
+    this.moniteAppElement.setAttribute('api-url', this.config.apiUrl);
+    this.moniteAppElement.setAttribute('component', component);
+    this.moniteAppElement.setAttribute('basename', '/');
+
+    // Add fetch-token script
+    const fetchTokenScript = document.createElement('script');
+    fetchTokenScript.setAttribute('slot', 'fetch-token');
+    fetchTokenScript.setAttribute('type', 'application/javascript');
+    fetchTokenScript.textContent = this.config.fetchToken.toString();
+    this.moniteAppElement.appendChild(fetchTokenScript);
+
+    // Add locale script
+    const localeScript = document.createElement('script');
+    localeScript.setAttribute('slot', 'locale');
+    localeScript.setAttribute('type', 'application/json');
+    localeScript.textContent = JSON.stringify(this.config.locale);
+    this.moniteAppElement.appendChild(localeScript);
+
+    // Add theme script
+    const themeScript = document.createElement('script');
+    themeScript.setAttribute('slot', 'theme');
+    themeScript.setAttribute('type', 'application/json');
+    themeScript.textContent = JSON.stringify(this.config.theme);
+    this.moniteAppElement.appendChild(themeScript);
+
+    // Add component settings script
+    const componentSettingsScript = document.createElement('script');
+    componentSettingsScript.setAttribute('slot', 'component-settings');
+    componentSettingsScript.setAttribute('type', 'application/json');
+    componentSettingsScript.textContent = JSON.stringify(
+      this.config.componentSettings
+    );
+    this.moniteAppElement.appendChild(componentSettingsScript);
+
+    return this;
+  }
+
+  public mount(container: HTMLElement) {
+    container.appendChild(this.moniteAppElement);
+    return this;
+  }
+}
 
 export {
   /** Custom element class for Monite App */
@@ -34,7 +135,7 @@ export {
 
   /** Gets the event target element for Monite App events */
   getMoniteAppEventTarget,
-};
 
-// eslint-disable-next-line import/no-default-export
-export default MoniteAppElement;
+  /** Monite Dropin factory function */
+  MoniteDropin,
+};
