70d9990009dee5d2cfca35f42ba79a31908446fd
Andrea <return.santana@gmail.com>
2025-03-12 22:36:07 +0100

    line items: using price helper

---
+++ packages/sdk-react/src/components/receivables/InvoiceDetails/CreateReceivable/sections/helpers.ts
@@ -0,0 +1,55 @@
+import { useCurrencies } from '@/core/hooks';
+
+export const usePriceHelper = () => {
+  const { formatCurrencyToDisplay, formatToMinorUnits } = useCurrencies();
+
+  const sanitizeAndFormatValue = (
+    inputValue: string,
+    currency: string,
+    isTyping: boolean
+  ) => {
+    let sanitizedValue = inputValue.replace(/[^0-9.,]/g, '');
+
+    // Handle comma and dot formatting for decimals
+    const lastCommaIndex = sanitizedValue.lastIndexOf(',');
+    const lastDotIndex = sanitizedValue.lastIndexOf('.');
+
+    if (
+      lastCommaIndex > -1 &&
+      (lastDotIndex === -1 || lastCommaIndex > lastDotIndex) &&
+      sanitizedValue.slice(lastCommaIndex + 1).length === 2 &&
+      !sanitizedValue.slice(lastCommaIndex + 1).includes(',')
+    ) {
+      sanitizedValue =
+        sanitizedValue.slice(0, lastCommaIndex) +
+        '.' +
+        sanitizedValue.slice(lastCommaIndex + 1);
+    }
+
+    const parsedValue = parseFloat(sanitizedValue.replace(/[^0-9.-]/g, ''));
+    const formattedToMinorUnits = formatToMinorUnits(sanitizedValue, currency);
+
+    if (
+      inputValue === '' ||
+      isNaN(parsedValue) ||
+      (formattedToMinorUnits && isNaN(formattedToMinorUnits))
+    ) {
+      return {
+        displayValue: formatCurrencyToDisplay(0, currency, false) || '',
+        minorUnitsValue: 0,
+      };
+    } else {
+      const newValue = formatCurrencyToDisplay(
+        formattedToMinorUnits || 0,
+        currency,
+        false
+      );
+      return {
+        displayValue: newValue || '',
+        minorUnitsValue: formattedToMinorUnits,
+      };
+    }
+  };
+
+  return { sanitizeAndFormatValue };
+};
