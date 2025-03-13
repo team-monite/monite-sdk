7c38ff5fe3d1412d39920a029e553874b98a6535
Andrea <return.santana@gmail.com>
2025-03-13 16:43:04 +0100

    line items: showing specific errors e.g. name, price

--- packages/sdk-react/src/components/receivables/InvoiceDetails/CreateReceivable/CreateReceivables.tsx
+++ packages/sdk-react/src/components/receivables/InvoiceDetails/CreateReceivable/CreateReceivables.tsx
@@ -241,10 +241,6 @@ const CreateReceivablesBase = ({
       (address) => address.id === shippingAddressId
     );
 
-    const filteredLineItems = values.line_items.filter((item) => {
-      return item.product?.name?.trim() !== '';
-    });
-
     const invoicePayload: Omit<
       components['schemas']['ReceivableFacadeCreateInvoicePayload'],
       'is_einvoice'
@@ -257,7 +253,7 @@ const CreateReceivablesBase = ({
 
       entity_bank_account_id: values.entity_bank_account_id || undefined,
       payment_terms_id: values.payment_terms_id,
-      line_items: filteredLineItems.map((item) => ({
+      line_items: values.line_items.map((item) => ({
         quantity: item.quantity,
         product: {
           name: item.product.name,
@@ -300,6 +296,19 @@ const CreateReceivablesBase = ({
         },
       }
     );
+  };
+
+  const handleFilterBeforeSubmit = (values: CreateReceivablesFormProps) => {
+    const filteredLineItems = values.line_items.filter((item) => {
+      return item.product?.name?.trim() !== '';
+    });
+
+    const updatedValues = {
+      ...values,
+      line_items: filteredLineItems,
+    };
+
+    handleCreateReceivable(updatedValues);
   };
 
   const { control } = useForm<CreateReceivablesProductsFormProps>({
@@ -752,7 +761,7 @@ const CreateReceivablesBase = ({
           <form
             id={formName}
             noValidate
-            onSubmit={handleSubmit(handleCreateReceivable)}
+            onSubmit={handleSubmit(handleFilterBeforeSubmit)}
             style={{ marginBottom: theme.spacing(7) }}
           >
             <Stack direction="column" spacing={7}>
