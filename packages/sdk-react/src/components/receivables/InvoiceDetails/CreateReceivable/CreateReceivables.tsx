217e74658815b9ed6c06f7b181a7eee3a1187d1f
Andrea <return.santana@gmail.com>
2025-03-14 11:39:55 +0100

    line items: fixing box-shadow cut

--- packages/sdk-react/src/components/receivables/InvoiceDetails/CreateReceivable/CreateReceivables.tsx
+++ packages/sdk-react/src/components/receivables/InvoiceDetails/CreateReceivable/CreateReceivables.tsx
@@ -745,9 +745,7 @@ const CreateReceivablesBase = ({
           <form
             id={formName}
             noValidate
-            onSubmit={handleSubmit((values) => {
-              handleCreateReceivable(values);
-            })}
+            onSubmit={handleSubmit(handleCreateReceivable)}
             style={{ marginBottom: theme.spacing(7) }}
           >
             <Stack direction="column" spacing={7}>
