b6edf14c03ece7cbf88c1003d4906630a04e18e2
Evgeniya Syropiatova <evgeniya.ogareva@gmail.com>
2025-04-17 09:23:38 +0200

    feat: types and exports

--- packages/sdk-react/src/components/aiAssistant/index.tsx
+++ packages/sdk-react/src/components/aiAssistant/index.tsx
@@ -1,1 +1,11 @@
 export * from './AIAssistant';
+
+export { AISidebar } from './components/AISidebar/AISidebar';
+export { AIChat } from './components/AIChat/AIChat';
+export { AIPrompts } from './components/AIPrompts/AIPrompts';
+export { AIStartScreen } from './components/AIStartScreen/AIStartScreen';
+
+export { AIAssistantChatProvider } from './context/AIAssistantChatContext';
+export { useAIAssistantOptions } from './hooks/useAIAssistantOptions';
+
+export * from './types';
