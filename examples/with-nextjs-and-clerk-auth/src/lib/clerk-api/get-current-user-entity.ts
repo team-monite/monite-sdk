de562f86282eeea7afe90f5fe90f0e8b929b630f
Costa <konstantin.pilyugin@monite.com>
2025-06-04 20:18:10 +0100

    feat(DEV-14115): change next js demo build time checks

--- examples/with-nextjs-and-clerk-auth/src/lib/clerk-api/get-current-user-entity.ts
+++ examples/with-nextjs-and-clerk-auth/src/lib/clerk-api/get-current-user-entity.ts
@@ -5,10 +5,16 @@ import { auth, clerkClient, currentUser } from '@clerk/nextjs/server';
 import { getEntityUserData } from '@/lib/clerk-api/get-entity-user-data';
 import { getOrganizationEntityData } from '@/lib/clerk-api/get-organization-entity';
 
-const IS_BUILD_PROCESS = process.env.NEXT_PHASE === 'phase-production-build';
+const isBuildTime = () => {
+  return (
+    process.env.NEXT_PHASE === 'phase-production-build' ||
+    (process.env.NODE_ENV === 'production' && !process.env.CLERK_SECRET_KEY) ||
+    (typeof window === 'undefined' && !global.fetch)
+  );
+};
 
 export const getCurrentUserEntity = async () => {
-  if (IS_BUILD_PROCESS) {
+  if (isBuildTime()) {
     return {
       entity_id: 'mock_entity_id_for_build',
       entity_user_id: 'mock_entity_user_id_for_build',
@@ -19,13 +25,21 @@ export const getCurrentUserEntity = async () => {
   const client = await clerkClient();
 
   const organization =
-    (orgId &&
-      (await client.organizations.getOrganization({
-        organizationId: orgId,
-      }))) ||
-    undefined;
+    orgId &&
+    (await client.organizations.getOrganization({
+      organizationId: orgId,
+    }));
 
-  const { entity_id } = getOrganizationEntityData(organization);
+  const entity_id =
+    organization && getOrganizationEntityData(organization)?.entity_id;
+
+  if (!entity_id) {
+    return {
+      entity_id: null,
+      entity_user_id: null,
+    };
+  }
+
   const user = await currentUser();
   const entityUserData = getEntityUserData(entity_id, user);
   const defaultEntityUserData = {
