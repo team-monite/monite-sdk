cec78589491b1e5847e1e239f017f7e6c01ec60c
Marc Perez <marc.perez@monite.com>
2025-07-08 16:01:19 +0200

    Adjust primary colors, add primaryForeground property

--- packages/sdk-react/src/core/theme/monite/index.ts
+++ packages/sdk-react/src/core/theme/monite/index.ts
@@ -9,6 +9,7 @@ export const getTheme = (theme: ThemeConfig) => {
 
     colors: {
       primary: theme.colors?.primary || '#3737FF',
+      primaryForeground: theme.colors?.primaryForeground || '#ffffff',
       secondary: theme.colors?.secondary || '#707070',
       neutral: theme.colors?.neutral || '#707070',
 
@@ -32,26 +33,31 @@ export const getTheme = (theme: ThemeConfig) => {
         fontSize: theme.typography?.h1?.fontSize ?? 48,
         fontWeight: theme.typography?.h1?.fontWeight ?? 600,
         lineHeight: theme.typography?.h1?.lineHeight ?? '68px',
+        textTransform: theme.typography?.h1?.textTransform ?? 'none',
       },
       h2: {
         fontSize: theme.typography?.h2?.fontSize ?? 34,
         fontWeight: theme.typography?.h2?.fontWeight ?? 640,
         lineHeight: theme.typography?.h2?.lineHeight ?? '40px',
+        textTransform: theme.typography?.h2?.textTransform ?? 'none',
       },
       h3: {
         fontSize: theme.typography?.h3?.fontSize ?? 24,
         fontWeight: theme.typography?.h3?.fontWeight ?? 600,
         lineHeight: theme.typography?.h3?.lineHeight ?? '32px',
+        textTransform: theme.typography?.h3?.textTransform ?? 'none',
       },
       subtitle1: {
         fontSize: theme.typography?.h2?.fontSize ?? 20,
         fontWeight: theme.typography?.h2?.fontWeight ?? 600,
         lineHeight: theme.typography?.h2?.lineHeight ?? '24px',
+        textTransform: theme.typography?.h2?.textTransform ?? 'none',
       },
       subtitle2: {
         fontSize: theme.typography?.h2?.fontSize ?? 18,
         fontWeight: theme.typography?.h2?.fontWeight ?? 600,
         lineHeight: theme.typography?.h2?.lineHeight ?? '24px',
+        textTransform: theme.typography?.h2?.textTransform ?? 'none',
       },
       body1: {
         fontSize: theme.typography?.body1?.fontSize ?? 16,
