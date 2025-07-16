536567344737e3fa1f66c719c6e07fb02d102033
Dinis Areias <dinis.areias@monite.com>
2025-07-16 16:58:46 +0100

    refactor: Remove unused React import in AutocompleteTags component

--- packages/sdk-react/src/components/approvalPolicies/ApprovalPolicyDetails/ApprovalPolicyForm/AutocompleteTags/AutocompleteTags.tsx
+++ packages/sdk-react/src/components/approvalPolicies/ApprovalPolicyDetails/ApprovalPolicyForm/AutocompleteTags/AutocompleteTags.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Control, Controller, useFormContext } from 'react-hook-form';
 
 import { useMoniteContext } from '@/core/context/MoniteContext';
