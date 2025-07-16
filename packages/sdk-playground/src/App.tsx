c323dcaa411a869228f338788bd95da11421903f
Marc Perez <marc.perez@monite.com>
2025-07-16 11:05:24 +0200

    Remove app css file (unused)

--- packages/sdk-playground/src/App.tsx
+++ packages/sdk-playground/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import './App.css';
 import { AppSidebar } from './components/app-sidebar';
 import { SidebarProvider } from './components/ui/sidebar';
 import {
