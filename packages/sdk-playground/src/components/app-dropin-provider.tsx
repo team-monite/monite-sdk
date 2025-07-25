bc52025ee1ea4598f263f84df6d63989453e2fa2
Marc Perez <marc.perez@monite.com>
2025-07-25 15:55:50 +0200

    Fix dropinConfig issue on sdk playground

--- packages/sdk-playground/src/components/app-dropin-provider.tsx
+++ packages/sdk-playground/src/components/app-dropin-provider.tsx
@@ -1,7 +1,7 @@
 import { fetchToken } from '@/services/fetch-token';
 import { getLoginEnvData } from '@/services/login-env-data';
 import { MoniteDropin } from '@monite/sdk-drop-in';
-import { useEffect, useRef } from 'react';
+import { useEffect, useMemo, useRef } from 'react';
 
 type AppDropinProvider = {
   component: string;
@@ -13,16 +13,19 @@ const AppDropinProvider = ({ component }: AppDropinProvider) => {
 
   const dropinRef = useRef<HTMLDivElement>(null);
 
-  const dropinConfig = {
-    entityId,
-    apiUrl,
-    fetchToken: () =>
-      fetchToken(apiUrl, {
-        entity_user_id: entityUserId,
-        client_id: clientId,
-        client_secret: clientSecret,
-      }),
-  };
+  const dropinConfig = useMemo(
+    () => ({
+      entityId,
+      apiUrl,
+      fetchToken: () =>
+        fetchToken(apiUrl, {
+          entity_user_id: entityUserId,
+          client_id: clientId,
+          client_secret: clientSecret,
+        }),
+    }),
+    [entityId, apiUrl, entityUserId, clientId, clientSecret]
+  );
 
   useEffect(() => {
     const dropin = new MoniteDropin(dropinConfig);
