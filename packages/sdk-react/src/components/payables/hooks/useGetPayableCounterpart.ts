63471642a29fff06c10edf6a8665b31b079dfae1
Dinis Areias <dinis.areias@monite.com>
2025-07-28 16:57:21 +0100

    feat: Add Payable hook to get Payable Counterpart, with AI suggestions

---
+++ packages/sdk-react/src/components/payables/hooks/useGetPayableCounterpart.ts
@@ -0,0 +1,98 @@
+import { components } from '@/api';
+import { useMoniteContext } from '@/core/context/MoniteContext';
+import { useCounterpartById } from '@/core/queries';
+import { useMemo } from 'react';
+
+export const useGetPayableCounterpart = ({
+  payable,
+}: {
+  payable: components['schemas']['PayableResponseSchema'];
+}) => {
+  // Heuristic to get the Counterpart (data or name) for the Payable
+  // A) Get Counterpart data from Payable data, if payable.counterpart_id
+  // B) Get Cunterpart data from AI suggestions, if !payable.counterpart_id
+  // C) Get just Counterpart name from raw OCR, if !payable.counterpart_id && !aiSuggestions?.suggested_counterpart?.id
+  const { api } = useMoniteContext();
+
+  // Get AI suggestions for the Payable, if not set payable.counterpart_id
+  const shouldFetchAISuggestions = useMemo(
+    () => !payable.counterpart_id,
+    [payable.counterpart_id]
+  );
+  const { data: AISuggestions, isLoading: isAISuggestionsLoading } =
+    api.payables.getPayablesIdSuggestions.useQuery(
+      {
+        path: { payable_id: payable.id },
+      },
+      {
+        enabled: shouldFetchAISuggestions,
+      }
+    );
+
+  // Determine the Counterpart ID to use, based on the AI suggestions and the Payable data
+  const counterpartId = useMemo(
+    () =>
+      payable.counterpart_id ??
+      AISuggestions?.suggested_counterpart?.id ??
+      null,
+    [payable.counterpart_id, AISuggestions?.suggested_counterpart?.id]
+  );
+
+  // Get the Counterpart data, based on the determined Counterpart ID
+  const { data: counterpart, isLoading: isCounterpartLoading } =
+    useCounterpartById(counterpartId ?? undefined);
+
+  // Check if the Counterpart raw OCR data name matches an existing Counterpart
+  const counterpartRawName = useMemo(
+    () => payable.counterpart_raw_data?.name,
+    [payable.counterpart_raw_data?.name]
+  );
+  const shouldCheckOCRMatching = useMemo(
+    () => Boolean(!counterpartId && counterpartRawName),
+    [counterpartId, counterpartRawName]
+  );
+  const {
+    data: isCounterpartMatchingToOCRFound,
+    isLoading: isCounterpartMatchingToOCRLoading,
+  } = api.counterparts.getCounterparts.useQuery(
+    {
+      query: {
+        counterpart_name__icontains: counterpartRawName,
+        is_vendor: true,
+        limit: 1,
+      },
+    },
+    {
+      enabled: shouldCheckOCRMatching,
+      select: (data) => Boolean(data.data.at(0)),
+    }
+  );
+
+  const isCounterpartAIMatched = useMemo(
+    () => Boolean(AISuggestions?.suggested_counterpart?.id),
+    [AISuggestions?.suggested_counterpart?.id]
+  );
+  const isCounterpartMatchingToOCRFoundBoolean = useMemo(
+    () => Boolean(isCounterpartMatchingToOCRFound),
+    [isCounterpartMatchingToOCRFound]
+  );
+  const isCounterpartLoadingCombined = useMemo(
+    () =>
+      isAISuggestionsLoading ||
+      isCounterpartMatchingToOCRLoading ||
+      isCounterpartLoading,
+    [
+      isAISuggestionsLoading,
+      isCounterpartMatchingToOCRLoading,
+      isCounterpartLoading,
+    ]
+  );
+
+  return {
+    counterpart,
+    counterpartRawName,
+    isCounterpartAIMatched,
+    isCounterpartMatchingToOCRFound: isCounterpartMatchingToOCRFoundBoolean,
+    isCounterpartLoading: isCounterpartLoadingCombined,
+  };
+};
