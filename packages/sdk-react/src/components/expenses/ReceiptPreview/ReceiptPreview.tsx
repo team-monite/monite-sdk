c95b91e19b664337bae2ef128fd0e277399cd6b5
Dinis Areias <dinis.areias@monite.com>
2025-09-04 18:48:36 +0100

    fix: Update ReceiptPreview layout and styling for improved responsiveness

--- packages/sdk-react/src/components/expenses/ReceiptPreview/ReceiptPreview.tsx
+++ packages/sdk-react/src/components/expenses/ReceiptPreview/ReceiptPreview.tsx
@@ -24,18 +24,16 @@ export const ReceiptPreview = ({
 
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
-      <DialogContent className="mtw:max-w-[90vw] mtw:sm:max-w-[720px] mtw:max-h-[90vh] mtw:p-0">
-        <DialogHeader className="mtw:p-6 mtw:pb-0">
-          <DialogTitle className="mtw:flex mtw:items-center mtw:justify-between">
-            {t(i18n)`Receipt ${receipt.document_id}`}
-          </DialogTitle>
-          <DialogDescription hidden>{t(
+      <DialogContent className="mtw:w-[744px] mtw:h-[744px] mtw:max-w-[90vw] mtw:max-h-[95vh] mtw:p-0 mtw:flex mtw:flex-col">
+        <DialogHeader className="mtw:p-6 mtw:pb-0 mtw:flex-shrink-0">
+          <DialogTitle>{t(i18n)`Receipt ${receipt.document_id}`}</DialogTitle>
+          <DialogDescription className="mtw:sr-only">{t(
             i18n
           )`Preview of receipt ${receipt.document_id}`}</DialogDescription>
         </DialogHeader>
-        <div className="mtw:flex-1 mtw:p-6 mtw:pt-0 mtw:overflow-hidden">
+        <div className="mtw:flex-1 mtw:p-6 mtw:pt-0 mtw:min-h-0 mtw:overflow-hidden">
           {receipt.file_url && (
-            <div className="mtw:h-full mtw:w-full">
+            <div className="mtw:h-full mtw:w-full mtw:flex mtw:items-center mtw:justify-center">
               <FileViewer
                 url={receipt.file_url}
                 mimetype={getMimetypeFromUrl(receipt.file_url)}
