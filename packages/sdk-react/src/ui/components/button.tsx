9bc236874bf3ca032eab35d5335186e855d586db
Dinis Areias <dinis.areias@monite.com>
2025-09-24 12:06:47 +0100

    fix: Add destructiveGhost variant to button component

--- packages/sdk-react/src/ui/components/button.tsx
+++ packages/sdk-react/src/ui/components/button.tsx
@@ -1,9 +1,7 @@
-import * as React from 'react';
-
 import { cn } from '@/ui/lib/utils';
 import { Slot } from '@radix-ui/react-slot';
-
 import { cva, type VariantProps } from 'class-variance-authority';
+import * as React from 'react';
 
 const buttonVariants = cva(
   'mtw:inline-flex mtw:cursor-pointer mtw:items-center mtw:justify-center mtw:gap-2 mtw:whitespace-nowrap mtw:rounded-md mtw:text-sm mtw:font-medium mtw:transition-all mtw:disabled:pointer-events-none mtw:disabled:opacity-50 mtw:[&_svg]:pointer-events-none mtw:[&_svg:not([class*=size-])]:size-4 mtw:shrink-0 mtw:[&_svg]:shrink-0 mtw:outline-none mtw:focus-visible:border-ring mtw:focus-visible:ring-ring/50 mtw:focus-visible:ring-[3px] mtw:aria-invalid:ring-destructive/20 mtw:dark:aria-invalid:ring-destructive/40 mtw:aria-invalid:border-destructive',
@@ -14,6 +12,7 @@ const buttonVariants = cva(
           'mtw:bg-primary mtw:text-primary-foreground mtw:shadow-xs mtw:hover:bg-primary/90',
         destructive:
           'mtw:bg-destructive mtw:text-white mtw:shadow-xs mtw:hover:bg-destructive/90 mtw:focus-visible:ring-destructive/20 mtw:dark:focus-visible:ring-destructive/40 mtw:dark:bg-destructive/60',
+        destructiveGhost: 'mtw:text-destructive mtw:hover:bg-destructive/20',
         outline:
           'mtw:border mtw:bg-background mtw:shadow-xs mtw:hover:bg-accent mtw:hover:text-accent-foreground mtw:dark:bg-input/30 mtw:dark:border-input mtw:dark:hover:bg-input/50',
         secondary:
