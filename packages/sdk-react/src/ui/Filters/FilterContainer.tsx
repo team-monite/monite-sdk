38eed2f5ac7a23e28e443f15baab800c22707a7e
Marc Perez <marc.perez@monite.com>
2025-10-01 11:20:29 +0200

    Fix flex grow on FilterContainer

--- packages/sdk-react/src/ui/Filters/FilterContainer.tsx
+++ packages/sdk-react/src/ui/Filters/FilterContainer.tsx
@@ -15,13 +15,14 @@ export const FilterContainer = ({
   return (
     <div
       className={classNames(
-        'mtw:flex mtw:flex-row mtw:justify-between mtw:items-center',
-        '[&>*]:mtw:flex-basis-fit-content [&>*]:mtw:flex-grow',
+        'Monite-Filters mtw:flex mtw:flex-row mtw:justify-between mtw:items-center',
         className
       )}
     >
-      {searchField}
-      <div className="mtw:flex mtw:flex-row mtw:gap-1 mtw:justify-end mtw:items-center mtw:ml-2">
+      <div className="mtw:flex-1 mtw:[&_.Monite-SearchField]:max-w-[400px] mtw:[&_.Monite-SearchField]:w-full">
+        {searchField}
+      </div>
+      <div className="Monite-Filters-Group mtw:flex mtw:flex-row mtw:gap-1 mtw:justify-end mtw:items-center mtw:ml-2 [&_.Monite-FilterControl]:mtw:max-w-[160px] [&_.Monite-FilterControl]:mtw:w-full">
         {children}
       </div>
     </div>
