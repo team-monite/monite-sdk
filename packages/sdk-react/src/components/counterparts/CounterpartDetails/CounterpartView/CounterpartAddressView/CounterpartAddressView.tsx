aebf96a9a6a2c8b784f2cead9d91ec3871ef36f5
Marc Perez <marc.perez@monite.com>
2025-10-02 13:34:31 +0200

    Optimize counterpart form by showing states as a dropdown

--- packages/sdk-react/src/components/counterparts/CounterpartDetails/CounterpartView/CounterpartAddressView/CounterpartAddressView.tsx
+++ packages/sdk-react/src/components/counterparts/CounterpartDetails/CounterpartView/CounterpartAddressView/CounterpartAddressView.tsx
@@ -1,19 +1,24 @@
+import {
+  useCounterpartAddressView,
+  CounterpartAddressViewProps,
+} from './useCounterpartAddressView';
 import { getCountries } from '@/core/utils/countries';
+import { USStatesEnum } from '@/enums/USStatesEnum';
 import { MoniteCard } from '@/ui/Card/Card';
 import { t } from '@lingui/macro';
 import { useLingui } from '@lingui/react';
 import EditIcon from '@mui/icons-material/Edit';
 import { CardActions, Button, Divider } from '@mui/material';
 
-import {
-  useCounterpartAddressView,
-  CounterpartAddressViewProps,
-} from './useCounterpartAddressView';
-
 export const CounterpartAddressView = (props: CounterpartAddressViewProps) => {
   const { i18n } = useLingui();
   const { onEdit } = useCounterpartAddressView(props);
   const { line1, line2, city, postal_code, state, country } = props.address;
+
+  const stateValue =
+    country === 'US' && USStatesEnum[state as keyof typeof USStatesEnum]
+      ? `${USStatesEnum[state as keyof typeof USStatesEnum]} — ${state}`
+      : state;
 
   return (
     <MoniteCard
@@ -36,7 +41,7 @@ export const CounterpartAddressView = (props: CounterpartAddressViewProps) => {
         },
         {
           label: t(i18n)`State`,
-          value: state,
+          value: stateValue,
         },
         {
           label: t(i18n)`Country`,
